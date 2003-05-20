c7e18f648788955297d07bc452d18c8dc027c277
pln <>
2003-05-20 21:07:39 +0000

    Minuit optimizer from CERN

---
+++ Likelihood/Minuit.h
@@ -0,0 +1,85 @@
+/** @file Minuit.h
+ * @brief Declaration for the Minuit Optimizer subclass.
+ * @author P. Nolan
+ *
+ */
+
+#ifndef MINUIT_H
+#define MINUIT_H
+
+#include "Likelihood/Optimizer.h"
+#include "Likelihood/Statistic.h"
+#include "Likelihood/cfortran.h"
+//#include "Likelihood/minuitfcn.h"
+#include "Likelihood/minuit.h"
+
+typedef long int logical;  // Copied from f2c.h
+typedef short ftnlen;
+
+namespace Likelihood {
+
+/** 
+ * @class Minuit
+ *
+ * @brief Wrapper class for the Minuit optimizer from CERN
+ *
+ * @author P. Nolan
+ *    
+ */
+
+class Minuit : public Optimizer {
+    
+public:
+    
+  Minuit(Statistic &stat);
+
+  virtual ~Minuit() {}
+
+  void find_min(int verbose = 0, double tol = 1e-3);
+
+  void setMaxEval(int);
+  int getQuality(void) const;
+  double getDistance(void) const;
+  std::vector<double> & getUncertainty(void) const;
+  
+  enum MinuitQuality {MINUIT_NOTCALC, MINUIT_DIAG, MINUIT_FORCEDPOS, 
+		      MINUIT_NORMAL};
+
+ private:
+  
+  Statistic *m_stat;
+  int doCmd(std::string command);
+  int m_maxEval;
+  int m_quality;
+  double m_distance;
+  std::vector<double> m_uncertainty;
+};
+
+ void fcn(int* npar, double* grad, double* fcnval,
+	  double* xval, int* iflag, void* futil);
+} // namespace Likelihood
+
+extern "C" {
+  void mninit_(int*, int*, int*);
+  void mnparm_( int *  num , const char * chnam, double * stval, 
+	        double * step,  double * bnd1 , 
+	        double * bnd2, int * ierror, ftnlen stringlen);
+  typedef void (mfcn)(int*, double *, double *, double *, int *, void *);
+  void mncomd_(mfcn *, const char * chstr, int * ierr, void * futil, 
+	       ftnlen stringlen);
+  void mnexcm_(mfcn *, char *, double *, int *, int *, void *, ftnlen);
+  void mnpout_(int *, char *, double *, double *, double *, double *, int *,
+	       ftnlen);
+  void mnstat_(double *, double *, double *, int *, int *, int *);
+  void mnseti_(char *, ftnlen);
+  void mnpars_(char *, int *, ftnlen);
+  void mnemat_(double * emat, int *ndim);
+  void mnerrs_(int *, double *, double *, double *, double *);
+  void mncont_(mfcn *, int *, int *, int *, double *, double *, int *,
+	       void *);
+  logical intrac_(double *);
+}
+
+#endif // MINUIT_H
+
+
---
+++ src/Minuit.cxx
@@ -0,0 +1,148 @@
+/** @file Minuit.cxx
+ * @brief Minuit class implementation
+ *
+ */
+
+#include <sstream>
+#include "Likelihood/Minuit.h"
+#include "Likelihood/Parameter.h"
+#include "Likelihood/LikelihoodException.h"
+
+namespace Likelihood {
+
+  Minuit::Minuit(Statistic& stat) : m_maxEval(200)
+  {m_stat = &stat;
+  MNINIT(0, 0, 0); // Initialize Minuit & set I/O unit numbers
+  }
+
+  void Minuit::setMaxEval(int n) {
+    m_maxEval = n;
+  }
+
+  int Minuit::getQuality(void) const {
+    return m_quality;
+  }
+
+  double Minuit::getDistance(void) const {
+    return m_distance;
+  }
+
+  std::vector<double> & Minuit::getUncertainty(void) const {
+    return m_uncertainty;
+  }
+
+  void Minuit::find_min(int verbose, double tol) {
+
+    std::vector<Parameter> params;
+    m_stat->getFreeParams(params);
+    int j=1;
+    int errorFlag;
+
+    int minuitVerbose = verbose - 1;
+    std::ostringstream pline;
+    pline << "SET PRINT " << minuitVerbose << std::endl;
+    doCmd(pline.str()); // Set verbosity of Minuit
+    doCmd("SET NOWARN");
+    // Tell Minuit about parameter values, names, bounds, etc.
+    for (std::vector<Parameter>::iterator p = params.begin();
+	 p != params.end(); p++, j++) {
+      double scale = 1.0; // Is this the best choice?
+      double value = p->getValue();
+      double lowerBound = p->getBounds().first;
+      double upperBound = p->getBounds().second;
+      mnparm_(&j, p->getName().c_str(), &value, &scale, 
+	      &lowerBound, &upperBound, &errorFlag, p->getName().size());
+    }
+
+    doCmd("SET ERR 0.5");  // Delta value = 1/2: correct for likelihood
+    doCmd("SET GRAD 1");  // Use gradient calculated by fcn
+    std::ostringstream mline;
+    mline << "MIGRAD " << m_maxEval << " " << tol << std::endl;
+    int retCode = doCmd(mline.str());  // Minimize fcn
+    if (retCode == 4) {
+      // Abnormal termination
+      throw LikelihoodException
+	("Minuit abnormal termination. (No convergence?)");
+    }
+    else if (retCode > 0) {
+      // Faulty command line
+      throw LikelihoodException("Minuit bad command line");
+    }
+
+    // Extract fitted parameters
+    if (verbose != 0) {
+      std::cout << "Final values: " << std::endl;
+    }
+    j = 1;
+    for (std::vector<Parameter>::iterator p = params.begin();
+	 p != params.end(); p++, j++) {
+      std::vector<char> pname(10);
+      double pval, error, bnd1, bnd2;
+      int ivarbl;
+      mnpout_(&j, &pname[0], &pval, &error, &bnd1, &bnd2, &ivarbl, 
+	      pname.size());
+      p->setValue(pval);
+      if (verbose != 0) {
+	std::cout << "  " << std::string(pname.begin(), pname.end()) 
+		  << " = " << pval << std::endl;
+      }
+    }
+    m_stat->setParams(params);
+
+    // Get information about quality of minimization
+    int nVariable, nparx, minStat;
+    double fmin, vertDist, errDef;
+    mnstat_(&fmin, &vertDist, &errDef, &nVariable, &nparx, &minStat);
+    m_quality = minStat;
+    m_distance = vertDist;
+    if (verbose != 0) {
+      std::cout << "Minuit fit quality: " << minStat << 
+	"   estimated distance: " << vertDist << std::endl;
+    }
+
+    // Get parameter uncertainties
+    if (verbose != 0) {
+      std::cout << "Minuit parameter uncertainties:" << std::endl;
+    }
+    m_uncertainty.clear();
+    for (int i = 1; i <= nVariable; i++) {
+      double ePlus, eMinus, eParab, globCC;
+      mnerrs_(&i, &ePlus, &eMinus, &eParab, &globCC);
+      m_uncertainty.push_back(eParab);  // Not using MINOS, so this is it.
+      if (verbose != 0) {
+	std::cout << "  " << i << "  " << eParab << std::endl;
+      }
+    }
+
+  } // End of find_min
+
+  int Minuit::doCmd(std::string command) {
+    // Pass a command string to Minuit
+    int errorFlag = 0;
+    void * statistic = static_cast<void *>(m_stat);
+    mncomd_(&fcn, command.c_str(), &errorFlag, statistic,
+	    command.length());
+    return errorFlag;
+  }
+
+  void fcn(int* npar, double* grad, double* fcnval,
+	   double* xval, int* iflag, void* futil) {
+    // This is the function that Minuit minimizes
+    std::vector<double> parameters(xval, xval+*npar);
+
+    // What a hack!  Minuit thinks futil is a function 
+    // pointer.  It's been hijacked to be a pointer to
+    // m_stat, so this non-member function can use it.
+    Statistic * statp = static_cast<Statistic *>(futil);
+    *fcnval = -statp->value(parameters);
+    if (*iflag == 2) { // Return gradient values
+      std::vector<double> gradient;
+      statp->getFreeDerivs(gradient);
+      for (int i=0; i < *npar; i++) {
+	grad[i] = -gradient[i];
+      }
+    }
+  }    
+} // namespace Likelihood
+
+
