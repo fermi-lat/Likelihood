93562ce5c4a0cb79d792510d162208a7693195ff
jchiang <>
2003-11-09 03:39:25 +0000

    add user guide to mainpage.h

--- src/mainpage.h
+++ src/mainpage.h
@@ -141,14 +141,13 @@
 
  <hr>
  @section notes Release Notes
- @verbinclude release.notes
+  release.notes
 
  <hr>
  @section requirements requirements
  @verbinclude requirements
 
  <hr> 
- @todo Error estimates, confidence regions, etc. 
  @todo Energy dispersion
  @todo Generalize Npred calculation, e.g., zenith angle cuts, fit-able 
        source locations
@@ -157,3 +156,169 @@
  @todo Analyze EGRET data
  @todo Boost.Python
  */
+
+/**
+ @page userGuide User's Guide
+
+ @section likeApp The likelihood application
+
+ Here is a sample session in which 1000 events of simulated data,
+ produced for two 3EG sources, 3C 279 and 3C 273, are analyzed:
+
+ @verbatim
+glast-guess1[jchiang] ~/SciTools/dev/Likelihood/v2r3/rh72_gcc2953/like_test.exe
+ROI cuts file [$(LIKELIHOODROOT)/xml/RoiCuts.xml] : 
+Spacecraft file [virgo_scFiles] : 
+Exposure file [none] : 
+Response functions to use <FRONT/BACK|COMBINED> [FRONT/BACK] : 
+Source model file [virgo_region.xml] : 
+Event file [virgo_region_events_0000.fits] : 
+Optimizer <LBFGS|MINUIT|DRMNGB> [DRMNGB] : 
+Optimizer verbosity [1] : 0
+Fit tolerance [0.0001] : 
+Source model output file [/dev/null] : 
+Use OptEM? [no] : 
+flux-style output file name (can be 'none') [flux_model.xml] : 
+Creating source named 3C 279
+Computing exposure at (193.98, -5.82).....................!
+Creating source named 3C 273
+Computing exposure at (187.25, 2.17).....................!
+adding source 3C 273
+adding source 3C 279
+LogLike::getEvents:
+Out of 1000 events in file virgo_region_events_0000.fits,
+ 854 were accepted, and 146 were rejected.
+
+Drmngb return code: 3
+3C 273:
+Prefactor: 2.52004 +/- 0.186379
+Index: -2.56014 +/- 0.0871032
+Scale: 100
+Npred: 227.436
+
+3C 279:
+Prefactor: 6.46561 +/- 0.298801
+Index: -1.90003 +/- 0.0296193
+Scale: 100
+Npred: 626.56
+Writing fitted model to /dev/null
+Writing flux-style xml model file to flux_model.xml
+glast-guess1[jchiang] 
+@endverbatim
+
+The
+<a href="http://glast.stanford.edu/cgi-bin/cvsweb/Likelihood/xml/RoiCuts.xml?cvsroot=CVS_SLAC">ROI 
+cuts file</a> contains basic information about the extraction region,
+valid time ranges, energies, etc.:
+
+@verbatim
+<?xml version='1.0' standalone='no'?>
+<!DOCTYPE Region-of-Interest SYSTEM "RoiCuts.dtd">
+<Region-of-Interest title="Virgo Region">
+   <timeInterval start="0"
+                 stop="7"
+                 unit="days"/>
+   <energies emin="30." 
+             emax="3.1623e5"
+             unit="MeV"/>
+   <acceptanceCone longitude="193.98"
+                   latitude="-5.82"
+                   radius="30"
+                   coordsys="Celestial"/>
+</Region-of-Interest>
+@endverbatim
+
+The Spacecraft file is either a single FITS file or a list of FITS files:
+
+@verbatim
+virgo_region_scData_0000.fits
+virgo_region_scData_0001.fits
+@endverbatim
+
+If there are diffuse components in the source model, then one must
+provide an exposure map that has been computed using the @ref expMap
+application.  If the data contain only point sources, then an exposure
+map can be omitted.
+
+Presently, only Glast25 Front/Back and Combined response functions are
+available.
+
+The Source model file contains an xml description of the various sources
+to be modeled.  Here's the source model file used in the above fit:
+
+@verbatim
+<source_library title="prototype sources" function_library="$(LIKELIHOODROOT)/xml/A1_Functions.xml">
+  <source name="3C 279" type="PointSource">
+    <spectrum type="PowerLaw">
+      <parameter max="1000" min="0.001" free="1" name="Prefactor" scale="1e-09" value="1" />
+      <parameter max="-1" min="-3.5" free="1" name="Index" scale="1" value="-2" />
+      <parameter max="200" min="50" free="0" name="Scale" scale="1" value="100" />
+    </spectrum>
+    <spatialModel type="SkyDirFunction">
+      <parameter max="3.40282e+38" min="-3.40282e+38" free="0" name="RA" scale="1" value="193.98" />
+      <parameter max="3.40282e+38" min="-3.40282e+38" free="0" name="DEC" scale="1" value="-5.82" />
+    </spatialModel>
+  </source>
+  <source name="3C 273" type="PointSource">
+    <spectrum type="PowerLaw">
+      <parameter max="1000" min="0.001" free="1" name="Prefactor" scale="1e-09" value="1" />
+      <parameter max="-1" min="-3.5" free="1" name="Index" scale="1" value="-2" />
+      <parameter max="200" min="50" free="0" name="Scale" scale="1" value="100" />
+    </spectrum>
+    <spatialModel type="SkyDirFunction">
+      <parameter max="3.40282e+38" min="-3.40282e+38" free="0" name="RA" scale="1" value="187.25" />
+      <parameter max="3.40282e+38" min="-3.40282e+38" free="0" name="DEC" scale="1" value="2.17" />
+    </spatialModel>
+  </source>
+</source_library>
+@endverbatim
+
+The Event file also can either be a single FITS file or a list.
+
+The optimizer package provides three optimizers from which to choose.
+Minuit and Drmngb provide error estimates based on covariance matrix
+computed from the inverse Hessian.
+
+After fitting the source model can be written to an output file.  If
+the same file name as the input source model file is given, then the
+file will be overwritten.
+
+OptEM is an alternative method for performing the optimization using
+the expectation maximization method.
+
+Finally, the source model can be written out as a flux-package style
+xml file that's suitable for use with either obsSim or Gleam.  Here's
+the resulting file for the above fit:
+
+@verbatim
+<source_library title="Likelihood_model">
+  <source flux="0.0105858" name="_3C_273">
+    <spectrum escale="MeV">
+      <particle name="gamma">
+        <power_law emax="316230" emin="30" gamma="2.56014" />
+      </particle>
+      <celestial_dir ra="187.25" dec="2.17" />
+    </spectrum>
+  </source>
+  <source flux="0.0212465" name="_3C_279">
+    <spectrum escale="MeV">
+      <particle name="gamma">
+        <power_law emax="316230" emin="30" gamma="1.90003" />
+      </particle>
+      <celestial_dir ra="193.98" dec="-5.82" />
+    </spectrum>
+  </source>
+  <source name="all_in_flux_model.xml">
+    <nestedSource sourceRef="_3C_273" />
+    <nestedSource sourceRef="_3C_279" />
+  </source>
+</source_library>
+@endverbatim
+
+ @section TsMapApp The TsMap application
+
+ @section expMapApp The expMap application
+
+*/
+
+
