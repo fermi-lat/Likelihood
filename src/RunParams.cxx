59dca7900dc536da4048a3031e24d7eaac292c3e
jchiang <>
2004-01-10 22:53:11 +0000

    refactor RunParams; add pil exception codes

--- src/RunParams.cxx
+++ src/RunParams.cxx
@@ -4,7 +4,7 @@
  * command-line parameters.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/RunParams.cxx,v 1.6 2003/12/06 23:46:37 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/RunParams.cxx,v 1.7 2004/01/06 00:10:28 jchiang Exp $
  */
 
 #include <fstream>
@@ -19,6 +19,8 @@ namespace Likelihood {
 
 RunParams::RunParams(int iargc, char* argv[]) {
 
+   errorCodes();
+
    hoops::IParFile * pf = hoops::PILParFileFactory().NewIParFile(argv[0]);
    try {
       pf->Load();
@@ -30,11 +32,16 @@ RunParams::RunParams(int iargc, char* argv[]) {
 
       delete pf;
    } catch (hoops::Hexception &eObj) {
-      if (eObj.Code() == -3003) {
-         std::cout << "Likelihood::RunParams: .par file " << argv[0]
-                   << " is not found.  Check your PFILES directory." 
-                   << std::endl;
-         assert(eObj.Code() != -3003);
+      if (m_pil_errors.count(eObj.Code())) {
+         std::cout << "PIL exception: " << eObj.Code() << "\n"
+                   << m_pil_errors[eObj.Code()];
+         if (eObj.Code() == -3003) {
+// This is obviously a fatal exception.
+            std::cout << argv[0] << ".par file is not found. "
+                      << "Check your PFILES directory." 
+                      << std::endl;
+            assert(eObj.Code() != -3003);
+         }
       }
       throw;
    }
@@ -42,6 +49,34 @@ RunParams::RunParams(int iargc, char* argv[]) {
 
 RunParams::~RunParams() {
    delete m_prompter;
+}
+
+void RunParams::errorCodes() {
+   m_pil_errors[-3000] = std::string("Null pointer passed as an argument, ")
+      + "and function does not allow it.";
+   m_pil_errors[-3001] = "Bad argument passed.";
+   m_pil_errors[-3002] = "Not enough memory.";
+   m_pil_errors[-3003] = "Cannot open/create file.";
+   m_pil_errors[-3004] = "Read from file failed.";
+   m_pil_errors[-3005] = "Write to file failed.";
+   m_pil_errors[-3006] = "Unexpected end of string.";
+   m_pil_errors[-3007] = "Invalid name of parameter.";
+   m_pil_errors[-3008] = "Invalid type of parameter in parameter file.";
+   m_pil_errors[-3009] = "Invalid mode of parameter in parameter file.";
+   m_pil_errors[-3010] = "Invalid line in parameter file encountered.";
+   m_pil_errors[-3011] = "Feature not implemented.";
+   m_pil_errors[-3012] = "File does not exist.";
+   m_pil_errors[-3013] = "File exists.";
+   m_pil_errors[-3014] = "File is not readable.";
+   m_pil_errors[-3015] = "File is not writable.";
+   m_pil_errors[-3016] = "Blank line encountered.";
+   m_pil_errors[-3017] = "Comment line encountered.";
+   m_pil_errors[-3018] = "Invalid line encountered.";
+   m_pil_errors[-3019] = "No such parameter.";
+   m_pil_errors[-3020] = "PFILES environment variable too long.";
+   m_pil_errors[-3021] = "PFILES environment variable is badly formatted.";
+   m_pil_errors[-3022] = "Cannot (un)lock parameter file.";
+   m_pil_errors[-3023] = "Bogus parameters found in command line.";
 }
 
 void RunParams::resolve_fits_files(std::string filename, 
--- src/test/expMap.cxx
+++ src/test/expMap.cxx
@@ -4,7 +4,7 @@
  * by the Likelihood tool.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/test/expMap.cxx,v 1.4 2003/12/05 05:39:23 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/test/expMap.cxx,v 1.5 2003/12/06 23:46:38 jchiang Exp $
  */
 
 #ifdef TRAP_FPE
@@ -30,6 +30,7 @@
 #include "Likelihood/Exception.h"
 
 using namespace Likelihood;
+using latResponse::irfsFactory;
 
 int main(int iargc, char* argv[]) {
 
@@ -63,22 +64,22 @@ int main(int iargc, char* argv[]) {
 // Create the response functions.
    std::string responseFuncs;
    params.getParam("Response_functions", responseFuncs);
-   latResponse::IrfsFactory irfsFactory;
+//   latResponse::IrfsFactory irfsFactory;
    if (responseFuncs == "COMBINED_G25") {
       ResponseFunctions::addRespPtr(4, 
-                                    irfsFactory.create("Glast25::Combined"));
+                                    irfsFactory().create("Glast25::Combined"));
    } else if (responseFuncs == "FRONT/BACK_G25") {
-      ResponseFunctions::addRespPtr(2, irfsFactory.create("Glast25::Front"));
-      ResponseFunctions::addRespPtr(3, irfsFactory.create("Glast25::Back"));
+      ResponseFunctions::addRespPtr(2, irfsFactory().create("Glast25::Front"));
+      ResponseFunctions::addRespPtr(3, irfsFactory().create("Glast25::Back"));
    } else if (responseFuncs == "TESTDC1") {
-      ResponseFunctions::addRespPtr(1, irfsFactory.create("DC1::test"));
+      ResponseFunctions::addRespPtr(1, irfsFactory().create("DC1::test"));
    } else if (responseFuncs == "FRONT") {
-      ResponseFunctions::addRespPtr(5, irfsFactory.create("DC1::Front"));
+      ResponseFunctions::addRespPtr(5, irfsFactory().create("DC1::Front"));
    } else if (responseFuncs == "BACK") {
-      ResponseFunctions::addRespPtr(6, irfsFactory.create("DC1::Back"));
+      ResponseFunctions::addRespPtr(6, irfsFactory().create("DC1::Back"));
    } else if (responseFuncs == "FRONT/BACK") {
-      ResponseFunctions::addRespPtr(5, irfsFactory.create("DC1::Front"));
-      ResponseFunctions::addRespPtr(6, irfsFactory.create("DC1::Back"));
+      ResponseFunctions::addRespPtr(5, irfsFactory().create("DC1::Front"));
+      ResponseFunctions::addRespPtr(6, irfsFactory().create("DC1::Back"));
    }
 
 // Set the source region radius.  This should be larger than the
