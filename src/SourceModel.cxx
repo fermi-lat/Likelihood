a9a6c672c8553070685dc5c9b2585afbba595c91
jchiang <>
2004-08-23 16:14:57 +0000

    write counts map in Equatorial coords

--- src/SourceModel.cxx
+++ src/SourceModel.cxx
@@ -3,7 +3,7 @@
  * @brief SourceModel class implementation
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SourceModel.cxx,v 1.44 2004/08/19 21:45:53 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SourceModel.cxx,v 1.45 2004/08/23 15:38:56 jchiang Exp $
  */
 
 #include <cmath>
@@ -11,6 +11,8 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+
+#include "fitsio.h"
 
 #include "xml/Dom.h"
 #include "xml/XmlParser.h"
@@ -35,6 +37,15 @@
 #include "Likelihood/ScData.h"
 #include "Likelihood/SourceModelBuilder.h"
 #include "Likelihood/SourceModel.h"
+
+namespace {
+   void fitsReportError(FILE *stream, int status) {
+      fits_report_error(stream, status);
+      if (status != 0) {
+         throw std::string("writeExposureFile: cfitsio error.");
+      }
+   }
+} // unnamed namespace
 
 namespace Likelihood {
 
@@ -488,9 +499,7 @@ void SourceModel::makeCountsMap(const std::string & filename,
       } // k
    } // j
 
-   ExposureMap::writeFitsFile(filename, longitudes, latitudes,
-                              energies, map, 0, 0);
-
+   writeFitsFile(filename, longitudes, latitudes, energies, map);
 }
 
 SourceModel::Aeff::Aeff(Source * src, astro::SkyDir & appDir, double energy,
@@ -512,6 +521,130 @@ double SourceModel::Aeff::operator()(double costheta) const {
                                 m_type);
    }
    return 0;
+}
+
+void SourceModel::writeFitsFile(const std::string &filename, 
+                                std::vector<double> &ras,
+                                std::vector<double> &decs,
+                                std::vector<double> &energies,
+                                std::vector< std::valarray<double> > &map) {
+   fitsfile *fptr;
+   int status = 0;
+   
+// Always overwrite an existing file.
+   std::remove(filename.c_str());
+   fits_create_file(&fptr, filename.c_str(), &status);
+   fitsReportError(stderr, status);
+
+   int bitpix = DOUBLE_IMG;
+   long naxis = 3;
+   long naxes[] = {ras.size(), decs.size(), energies.size()};
+   fits_create_img(fptr, bitpix, naxis, naxes, &status);
+   fitsReportError(stderr, status);
+
+// Write the exposure map data.
+   long group = 0;
+   long dim1 = ras.size();
+   long dim2 = decs.size();
+
+// Repack exposure into a C array.
+   double *map_array = new double[ras.size()*decs.size()*energies.size()];
+   int indx = 0;
+   for (unsigned int k = 0; k < energies.size(); k++) {
+      for (unsigned int i = 0; i < map[k].size(); i++) {
+         map_array[indx] = map[k][i];
+         indx++;
+      }
+   }
+
+   fits_write_3d_dbl(fptr, group, dim1, dim2, 
+                     ras.size(), decs.size(), energies.size(),
+                     map_array, &status);
+   delete[] map_array;
+   fitsReportError(stderr, status);
+
+// Write some keywords.
+   double l0 = ras[0];
+   fits_update_key(fptr, TDOUBLE, "CRVAL1", &l0, 
+                   "longitude of reference pixel", &status);
+   fitsReportError(stderr, status);
+   double b0 = decs[0];
+   fits_update_key(fptr, TDOUBLE, "CRVAL2", &b0, 
+                   "latitude of reference pixel", &status);
+   fitsReportError(stderr, status);
+   
+   double lstep = ras[1] - ras[0];
+   fits_update_key(fptr, TDOUBLE, "CDELT1", &lstep, 
+                   "longitude step at ref. pixel", &status);
+   fitsReportError(stderr, status);
+   double bstep = decs[1] - decs[0];
+   fits_update_key(fptr, TDOUBLE, "CDELT2", &bstep, 
+                   "latitude step at ref. pixel", &status);
+   fitsReportError(stderr, status);
+   
+   float crpix1 = 1.0;
+   fits_update_key(fptr, TFLOAT, "CRPIX1", &crpix1, 
+                   "reference pixel for longitude coordinate", &status);
+   fitsReportError(stderr, status);
+   float crpix2 = 1.0;
+   fits_update_key(fptr, TFLOAT, "CRPIX2", &crpix2, 
+                   "reference pixel for latitude coordinate", &status);
+   fitsReportError(stderr, status);
+   
+   char *ctype1 = "RA---CAR";
+   fits_update_key(fptr, TSTRING, "CTYPE1", ctype1, 
+                   "right ascension", &status);
+   fitsReportError(stderr, status);
+   char *ctype2 = "DEC--CAR";
+   fits_update_key(fptr, TSTRING, "CTYPE2", ctype2, 
+                   "declination", &status);
+   fitsReportError(stderr, status);
+
+   double logEmin = log(energies[0]);
+   fits_update_key(fptr, TDOUBLE, "CRVAL3", &logEmin,
+                   "reference value for log_energy coordinate", &status);
+   fitsReportError(stderr, status);
+
+   int nee = energies.size();
+   double estep = log(energies[nee-1]/energies[0])/(nee-1);
+   fits_update_key(fptr, TDOUBLE, "CDELT3", &estep, 
+                   "step in log_energy coordinate", &status);
+   fitsReportError(stderr, status);
+
+   float crpix3 = 1.;
+   fits_update_key(fptr, TFLOAT, "CRPIX3", &crpix3,
+                   "reference pixel for log_energy coordinate", &status);
+   fitsReportError(stderr, status);
+
+   char *ctype3 = "log_MeV";
+   fits_update_key(fptr, TSTRING, "CTYPE3", ctype3,
+                   "units for log_energy", &status);
+   fitsReportError(stderr, status);
+
+   
+// Write the energy array as a binary table.
+   int nrows = energies.size();
+   int tfields = 1;
+   char *ttype[] = {"Energy"};
+   char *tform[] = {"1D"};
+   char *tunit[] = {"MeV"};
+   char extname[] = "True Photon Energy Array";
+   
+   int firstrow  = 1;
+   int firstelem = 1;
+   
+   fits_create_tbl(fptr, BINARY_TBL, nrows, tfields, ttype, tform,
+                   tunit, extname, &status);
+   fitsReportError(stderr, status);
+   
+   fits_write_col(fptr, TDOUBLE, 1, firstrow, firstelem, nrows, 
+                  &energies[0], &status);
+   fitsReportError(stderr, status);
+   
+   fits_close_file(fptr, &status);
+   fitsReportError(stderr, status);
+   
+   return;
 }
 
 } // namespace Likelihood
