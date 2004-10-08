f62082a55a0fa50f412cf4e383fb95c5eeb00507
jchiang <>
2004-10-08 05:59:22 +0000

    refine unit test; use pointer for m_dataMap

--- Likelihood/SourceMap.h
+++ Likelihood/SourceMap.h
@@ -4,7 +4,7 @@
  *        instrument response.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/SourceMap.h,v 1.7 2004/10/07 04:43:39 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/SourceMap.h,v 1.8 2004/10/08 00:28:49 jchiang Exp $
  */
 
 #ifndef Likelihood_SourceMap_h
@@ -22,20 +22,22 @@ class CountsMap;
 /*
  * @class SourceMap
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/SourceMap.h,v 1.7 2004/10/07 04:43:39 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/SourceMap.h,v 1.8 2004/10/08 00:28:49 jchiang Exp $
  */
 
 class SourceMap {
 
 public:
 
-   SourceMap(Source * src, const CountsMap & dataMap);
+   SourceMap(Source * src, const CountsMap * dataMap);
 
    SourceMap(const std::string & sourceMapsFile, const std::string & srcName);
 
    ~SourceMap() {
-      delete s_meanPsf;
-      delete s_binnedExposure;
+/// @todo need to reference count this
+//       delete s_meanPsf;
+//       delete s_binnedExposure;
+      delete m_dataMap;
    }
 
    const std::vector<double> & model() const {return m_model;}
@@ -52,7 +54,7 @@ private:
 
    std::string m_name;
 
-   const CountsMap & m_dataMap;
+   const CountsMap * m_dataMap;
 
 /// @brief m_models has the same size as the data in the dataMap plus
 /// one energy plane.
