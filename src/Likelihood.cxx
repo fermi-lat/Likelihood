c7ea9ef5deaf8c91a1c000a457fd978a28ee986b
cvs2git <>
2004-11-11 20:27:44 +0000

    This commit was manufactured by cvs2svn to create tag 'v5r3p1'.
    Sprout from master 2004-11-11 20:27:43 UTC jchiang 'access to binned exposure'
    Delete:
        .cvsignore
        ChangeLog
        Likelihood/AppHelpers.h
        Likelihood/BinnedExposure.h
        Likelihood/BinnedLikelihood.h
        Likelihood/CountsMap.h
        Likelihood/DiffuseSource.h
        Likelihood/Event.h
        Likelihood/EventArg.h
        Likelihood/Exception.h
        Likelihood/ExposureCube.h
        Likelihood/ExposureMap.h
        Likelihood/FitsImage.h
        Likelihood/FluxBuilder.h
        Likelihood/HistND.h
        Likelihood/LikeExposure.h
        Likelihood/LogLike.h
        Likelihood/MapShape.h
        Likelihood/MeanPsf.h
        Likelihood/Npred.h
        Likelihood/OneSourceFunc.h
        Likelihood/OptEM.h
        Likelihood/Pixel.h
        Likelihood/PointSource.h
        Likelihood/ResponseFunctions.h
        Likelihood/RoiCuts.h
        Likelihood/ScData.h
        Likelihood/SkyDirArg.h
        Likelihood/SkyDirFunction.h
        Likelihood/Source.h
        Likelihood/SourceFactory.h
        Likelihood/SourceMap.h
        Likelihood/SourceModel.h
        Likelihood/SourceModelBuilder.h
        Likelihood/SpatialMap.h
        Likelihood/SpectrumFactory.h
        Likelihood/SrcArg.h
        Likelihood/TrapQuad.h
        Likelihood/Util.h
        Likelihood/XmlBuilder.h
        Likelihood/logSrcModel.h
        cmt/.cvsignore
        cmt/Makefile
        cmt/requirements
        data/Crab_model.xml
        data/LatCountsMapTemplate
        data/RoiCuts.xml
        data/Virgo_region.xml
        data/Virgo_region_2.xml
        data/anticenter_Roi.xml
        data/anticenter_expMap.fits
        data/anticenter_model.xml
        data/anticenter_model_bpl.xml
        data/expMap_0.fits
        data/expMap_1.fits
        data/expMap_2.fits
        data/expMap_3.fits
        data/expMap_4.fits
        data/galdiffuse_events_0000.fits
        data/oneday_events_0000.fits
        data/oneday_scData_0000.fits
        data/single_src_events_0000.fits
        doc/Doxyfile
        doc/Makefile
        doc/images/TsMap.png
        doc/images/TsMap_compare.png
        doc/images/Virgo_map.png
        doc/images/allsky.png
        doc/images/allsky_3EG.png
        doc/images/anticenter_1day.png
        doc/images/expMap.png
        doc/release.notes
        pfiles/TsMap.par
        pfiles/diffuseResponses.par
        pfiles/expMap.par
        pfiles/exposureMap.par
        pfiles/gtcntsmap.par
        pfiles/gtsrcmaps.par
        pfiles/makeExposureCube.par
        pfiles/meanPsf.par
        python/Likelihood.i
        python/Makefile
        python/test.py
        src/AppHelpers.cxx
        src/BinnedExposure.cxx
        src/BinnedLikelihood.cxx
        src/CountsMap.cxx
        src/DiffuseSource.cxx
        src/Event.cxx
        src/ExposureCube.cxx
        src/ExposureMap.cxx
        src/FitsImage.cxx
        src/FluxBuilder.cxx
        src/HistND.cxx
        src/LikeExposure.cxx
        src/Likelihood.cxx
        src/LogLike.cxx
        src/MeanPsf.cxx
        src/Npred.cxx
        src/OneSourceFunc.cxx
        src/OptEM.cxx
        src/Pixel.cxx
        src/PointSource.cxx
        src/ResponseFunctions.cxx
        src/RoiCuts.cxx
        src/SConstruct
        src/ScData.cxx
        src/SkyDirFunction.cxx
        src/Source.cxx
        src/SourceFactory.cxx
        src/SourceMap.cxx
        src/SourceModel.cxx
        src/SourceModelBuilder.cxx
        src/SpatialMap.cxx
        src/SpectrumFactory.cxx
        src/TrapQuad.cxx
        src/TsMap/TsMap.cxx
        src/XmlBuilder.cxx
        src/buildBase
        src/counts_map/counts_map.cxx
        src/diffuseResponses/diffuseResponses.cxx
        src/expMap/expMap.cxx
        src/exposureMap/exposureMap.cxx
        src/gtcntsmap/gtcntsmap.cxx
        src/gtsrcmaps/gtsrcmaps.cxx
        src/likelihood/EasyPlot.cxx
        src/likelihood/EasyPlot.h
        src/likelihood/likelihood.cxx
        src/logSrcModel.cxx
        src/mainpage.h
        src/makeExposureCube/makeExposureCube.cxx
        src/map.py
        src/meanPsf/meanPsf.cxx
        src/old_tests/main.cxx
        src/test/Data/gas.cel
        src/test/SourceData.cxx
        src/test/SourceData.h
        src/test/XmlDiff.cxx
        src/test/XmlDiff.h
        src/test/test.cxx
        src/tutorial.h
        xml/A1_Functions.dtd
        xml/A1_Functions.xml
        xml/A1_Sources.dtd
        xml/A1_Sources.xml
        xml/A1_Srcs_noFuncLib.xml
        xml/RoiCuts.dtd
        xml/RoiCuts.xml

--- src/Likelihood.cxx
+++
@@ -1,8 +0,0 @@
-namespace Likelihood {
-
-// A dummy file used to fool CMT into building the SWIG generated
-// Python extension modules.
-//
-// Do not put any real code in this file.
-
-}
--- src/map.py
+++
@@ -1,7 +0,0 @@
-class map(dict):
-    def __init__(self):
-        dict.__init__(self)
-        self.ordered_keys = []
-    def __setitem__(self, key, value):
-        dict.__setitem__(self, key, value)
-        self.ordered_keys.append(key)
