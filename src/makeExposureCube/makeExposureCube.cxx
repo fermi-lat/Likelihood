3d46c94515c513ca9f902998bad050dbf6b7074b
jchiang <>
2004-11-28 15:01:29 +0000

    delete file to be clobbered rather than using flawed '!' syntax

--- src/makeExposureCube/makeExposureCube.cxx
+++ src/makeExposureCube/makeExposureCube.cxx
@@ -3,7 +3,7 @@
  * @brief Create an Exposure hypercube.
  * @author J. Chiang
  *
- *  $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/makeExposureCube/makeExposureCube.cxx,v 1.11 2004/09/03 21:46:19 jchiang Exp $
+ *  $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/makeExposureCube/makeExposureCube.cxx,v 1.12 2004/11/28 06:58:23 jchiang Exp $
  */
 
 #include <cstdlib>
@@ -33,7 +33,7 @@
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/makeExposureCube/makeExposureCube.cxx,v 1.11 2004/09/03 21:46:19 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/makeExposureCube/makeExposureCube.cxx,v 1.12 2004/11/28 06:58:23 jchiang Exp $
  */
 class ExposureCube : public st_app::StApp {
 public:
@@ -74,9 +74,10 @@ st_app::StAppFactory<ExposureCube> myAppFactory;
 
 void ExposureCube::run() {
    std::string output_file = m_pars["outfile"];
-   if (m_pars["clobber"]) {
-      output_file = "!" + output_file;
-   } else if (st_facilities::Util::fileExists(output_file)) {
+   if (m_pars["clobber"] &&
+       st_facilities::Util::fileExists(output_file)) {
+      std::remove(output_file.c_str());
+   } else {
       std::cout << "Output file " << output_file 
                 << " already exists and you have set 'clobber' to 'no'.\n"
                 << "Please provide a different output file name." 
