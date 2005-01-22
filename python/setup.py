7d4d2efd470c19b3eced21f93a4e0559752fc8c0
jchiang <>
2005-01-22 00:27:10 +0000

    using root 4; refactor setup.py; add exception handling

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,18 @@
 import sys, os
 
-module = sys.argv[1].split('.in')[0]
+def replaceEnvVars(target, source, env=None):
+    ifile = open(str(source[0]), 'r')
+    outfile = open(str(target[0]), 'w')
+    for line in ifile:
+        if line.find('$(') != -1:
+            package = line.split('$(')[1].split(')')[0]
+            outfile.write(line.replace('$('+package+')', os.environ[package]))
+        else:
+            outfile.write(line)
 
-ifile = open(module + ".in", 'r')
-outfile = open(module + ".i", 'w')
-for line in ifile:
-    if line.find('$(') != -1:
-        package = line.split('$(')[1].split(')')[0]
-        outfile.write(line.replace('$('+package+')', os.environ[package]))
-    else:
-        outfile.write(line)
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        outfile = sys.argv[1].replace('.in', '.i')
+        replaceEnvVars((outfile, ), (sys.argv[1], ))
+    elif len(sys.argv) == 3:
+        replaceEnvVars((sys.argv[2],), (sys.argv[1],))
