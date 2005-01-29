458af0bb4b1b5b8302ba51ae74feebdbd7ecb56f
jchiang <>
2005-01-29 16:01:47 +0000

    add swig build

--- src/expMap/expMap.cxx
+++ src/expMap/expMap.cxx
@@ -4,7 +4,7 @@
  * by the Likelihood tool.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/expMap/expMap.cxx,v 1.17 2005/01/11 15:32:59 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/expMap/expMap.cxx,v 1.18 2005/01/19 02:15:01 jchiang Exp $
  */
 
 #include <cmath>
@@ -40,7 +40,7 @@ using namespace Likelihood;
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/expMap/expMap.cxx,v 1.17 2005/01/11 15:32:59 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/expMap/expMap.cxx,v 1.18 2005/01/19 02:15:01 jchiang Exp $
  */
 class ExpMap : public st_app::StApp {
 public:
@@ -84,10 +84,9 @@ void ExpMap::promptForParameters() {
    m_pars.Prompt("scfile");
    m_pars.Prompt("exposure_cube_file");
    std::string expCubeFile = m_pars["exposure_cube_file"];
-   std::string expCubeTable = m_pars["exposure_cube_table"];
    if (expCubeFile != "none") {
       AppHelpers::checkTimeCuts(m_pars["evfile"], "EVENTS",
-                                m_pars["exposure_cube_file"], expCubeTable);
+                                m_pars["exposure_cube_file"], "");
    }
    m_pars.Prompt("outfile");
    AppHelpers::checkOutputFile(m_pars["clobber"], m_pars["outfile"]);
