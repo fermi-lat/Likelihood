9d31bc2fee469ca230acb7cc45b3c9758092360e
jchiang <>
2005-07-19 03:43:29 +0000

    remove python modules

--- python/load_data.py
+++
@@ -1,8 +0,0 @@
-import os
-import Likelihood
-
-LikelihoodRoot = os.environ['LIKELIHOODROOT']
-
-Likelihood.RoiCuts_setCuts('RoiCuts.xml')
-Likelihood.ScData_readData('test_scData_0000.fits')
-Likelihood.LogLike_loadResponseFunctions("FRONT/BACK")
