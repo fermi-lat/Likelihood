a3c2f43eefa15b0c7e01d2d4058922e2f9c65967
jchiang <>
2005-10-07 06:03:41 +0000

    use wcslib in ExposureMap

--- src/BinnedExposure.cxx
+++ src/BinnedExposure.cxx
@@ -4,7 +4,7 @@
  * various energies.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/BinnedExposure.cxx,v 1.10 2005/07/13 17:17:43 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/BinnedExposure.cxx,v 1.11 2005/10/03 15:02:43 jchiang Exp $
  */
 
 #include <cmath>
@@ -18,60 +18,36 @@
 #include "tip/IFileSvc.h"
 #include "tip/Image.h"
 
+#include "astro/SkyProj.h"
+
+#include "st_facilities/FitsImage.h"
+
 #include "Likelihood/BinnedExposure.h"
-#include "Likelihood/CountsMap.h"
-#include "Likelihood/EquinoxRotation.h"
 #include "Likelihood/Observation.h"
 
 #include "Verbosity.h"
 
 namespace Likelihood {
 
-BinnedExposure::BinnedExposure() : m_observation(0) {}
+BinnedExposure::BinnedExposure() : m_observation(0), m_proj(0) {}
 
 BinnedExposure::BinnedExposure(const std::vector<double> & energies,
                                const Observation & observation) 
-   : m_observation(&observation), m_energies(energies) {
+   : m_observation(&observation), m_energies(energies), m_proj(0) {
    computeMap();
 }
 
-BinnedExposure::BinnedExposure(const std::string & filename) {
+BinnedExposure::BinnedExposure(const std::string & filename) 
+   : m_observation(0), m_proj(0) {
+   m_proj = st_facilities::FitsImage::skyProjCreate(filename);
+
    std::auto_ptr<const tip::Image> 
       image(tip::IFileSvc::instance().readImage(filename, ""));
 
    m_exposureMap.clear();
    image->get(m_exposureMap);
 
-   const tip::Header & header = image->getHeader();
-
-   /// @bug tip doesn't allow direct access to NAXIS[1-3] keywords
-   /// through the Header, so we are forced to use
-   /// getImageDimensions() method.
-   std::vector<tip::PixOrd_t> dims = image->getImageDimensions();
-
-   unsigned int nra = dims[0];
-   double ra0;
-   double rastep;
-   double refpix1;
-   header["CRVAL1"].get(ra0);
-   header["CDELT1"].get(rastep);
-   header["CRPIX1"].get(refpix1);
-   m_ras.resize(nra);
-   for (unsigned int i = 0; i < nra; i++) {
-      m_ras.at(i) = (i - refpix1)*rastep + ra0;
-   }
-   
-   unsigned int ndec = dims[1];
-   double dec0;
-   double decstep;
-   double refpix2;
-   header["CRVAL2"].get(dec0);
-   header["CDELT2"].get(decstep);
-   header["CRPIX2"].get(refpix2);
-   m_decs.resize(ndec);
-   for (unsigned int i = 0; i < ndec; i++) {
-      m_decs.at(i) = (i - refpix2)*decstep + dec0;
-   }
+   m_naxes = image->getImageDimensions();
 
    std::auto_ptr<const tip::Table>
       energies(tip::IFileSvc::instance().readTable(filename, "Energies"));
@@ -100,67 +76,48 @@ double BinnedExposure::operator()(double energy, double ra, double dec) const {
       throw std::runtime_error(what.str());
    }
    unsigned int k = ie - m_energies.begin();
-   if (ra < 0) ra += 360.; 
-   if (ra > 360.) ra = fmod(ra, 360.);
-   unsigned int i = findIndex(m_ras.begin(), m_ras.end(), ra);
-   unsigned int j = findIndex(m_decs.begin(), m_decs.end(), dec);
-   unsigned int indx = (k*m_decs.size() + j)*m_ras.size() + i;
-   return m_exposureMap.at(indx);
-}
 
-void BinnedExposure::
-getRotatedImage(double energy, 
-                const std::vector<double> & lons, 
-                const std::vector<double> & lats,
-                const EquinoxRotation & rot,
-                std::vector< std::vector<double> > & image) const {
-   image.clear();
-   image.reserve(lons.size());
-   for (unsigned int i = 0; i < lons.size(); i++) {
-      std::vector<double> row;
-      row.reserve(lats.size());
-      for (unsigned int j = 0; j < lats.size(); j++) {
-         astro::SkyDir mapDir(lons.at(i), lats.at(j));
-         astro::SkyDir trueDir;
-         rot.do_rotation(mapDir, trueDir, false);
-         row.push_back((*this)(energy, trueDir.ra(), trueDir.dec()));
-      }
-      image.push_back(row);
-   }
-}
+   std::pair<double, double> pixel = m_proj->sph2pix(ra, dec);
 
-unsigned int 
-BinnedExposure::findIndex(std::vector<double>::const_iterator begin,
-                          std::vector<double>::const_iterator end,
-                          double value) const {
-   std::vector<double>::const_iterator it 
-      = std::upper_bound(begin, end, value);
-   if (it == end) {
-      throw std::range_error("BinnedExposure::findIndex: out of range value.");
+   int i = static_cast<int>(pixel.first) - 1;
+   int j = static_cast<int>(pixel.second) - 1;
+
+   unsigned int indx = (k*m_naxes.at(1) + j)*m_naxes.at(0) + i;
+
+   try {
+      return m_exposureMap.at(indx);
+   } catch (std::out_of_range &) {
+      return 0;
    }
-   int indx = it - begin - 1;
-   return indx;
 }
 
 void BinnedExposure::computeMap() {
-   linearArray(0., 360., 360, m_ras);
-   linearArray(-90., 90., 180, m_decs);
+   m_naxes.push_back(360);
+   m_naxes.push_back(180);
+   m_naxes.push_back(m_energies.size());
 
-   m_exposureMap.resize(m_ras.size()*m_decs.size()*m_energies.size(), 0);
+   double crpix[] = {m_naxes.at(0)/2 + 0.5, m_naxes.at(1)/2 + 0.5};
+   double crval[] = {180., 0};
+   double cdelt[] = {-1, 1};
+   double crota2(0);
+
+   m_proj = new astro::SkyProj("CAR", crpix, crval, cdelt, crota2, false);
+
+   m_exposureMap.resize(m_naxes.at(0)*m_naxes.at(1)*m_energies.size(), 0);
    int iter(0);
    if (print_output()) {
       std::cerr << "Computing binned exposure map";
    }
-   for (unsigned int j = 0; j < m_decs.size(); j++) {
-      for (unsigned int i = 0; i < m_ras.size(); i++) {
-         if ( print_output() && 
-              (iter % ((m_decs.size()*m_ras.size())/20)) == 0 ) {
+   for (int j = 0; j < m_naxes.at(1); j++) {
+      for (int i = 0; i < m_naxes.at(0); i++) {
+         if (print_output() && 
+             (iter % ((m_naxes.at(1)*m_naxes.at(0))/20)) == 0) {
             std::cerr << ".";
          }
-         astro::SkyDir dir(m_ras[i], m_decs[j], astro::SkyDir::EQUATORIAL);
+         std::pair<double, double> coord = m_proj->pix2sph(i + 1, j + 1);
+         astro::SkyDir dir(coord.first, coord.second);
          for (unsigned int k = 0; k < m_energies.size(); k++) {
-            unsigned int indx 
-               = k*m_ras.size()*m_decs.size() + j*m_ras.size() + i;
+            unsigned int indx = (k*m_naxes.at(1) + j)*m_naxes.at(0) + i;
             for (int evtType = 0; evtType < 2; evtType++) {
                Aeff aeff(m_energies[k], evtType, *m_observation);
                m_exposureMap.at(indx)
@@ -172,14 +129,6 @@ void BinnedExposure::computeMap() {
    }
    if (print_output()) {
       std::cerr << "!" << std::endl;
-   }
-}
-
-void BinnedExposure::linearArray(double xmin, double xmax, unsigned int npts,
-                                 std::vector<double> &xx) const {
-   double xstep = (xmax - xmin)/(npts - 1.);
-   for (unsigned int i = 0; i < npts; i++) {
-      xx.push_back(i*xstep + xmin);
    }
 }
 
@@ -202,13 +151,8 @@ double BinnedExposure::Aeff::operator()(double cosTheta) const {
 void BinnedExposure::writeOutput(const std::string & filename) const {
    std::remove(filename.c_str());
 
-   std::vector<long> dims(3);
-   dims.at(0) = m_ras.size();
-   dims.at(1) = m_decs.size();
-   dims.at(2) = m_energies.size();
-
    std::string ext("PRIMARY");
-   tip::IFileSvc::instance().appendImage(filename, ext, dims);
+   tip::IFileSvc::instance().appendImage(filename, ext, m_naxes);
    tip::Image * image = tip::IFileSvc::instance().editImage(filename, ext);
 
    image->set(m_exposureMap);
@@ -220,14 +164,14 @@ void BinnedExposure::writeOutput(const std::string & filename) const {
    header["DATE-OBS"].set("");
    header["DATE-END"].set("");
 
-   header["CRVAL1"].set(m_ras[0]);
-   header["CRPIX1"].set(0);
-   header["CDELT1"].set(m_ras[1] - m_ras[0]);
+   header["CRVAL1"].set(180.);
+   header["CRPIX1"].set(m_naxes.at(0)/2 + 0.5);
+   header["CDELT1"].set(-1.);
    header["CTYPE1"].set("RA---CAR");
 
-   header["CRVAL2"].set(m_decs[0]);
-   header["CRPIX2"].set(0);
-   header["CDELT2"].set(m_decs[1] - m_decs[0]);
+   header["CRVAL2"].set(0);
+   header["CRPIX2"].set(m_naxes.at(1)/2 + 0.5);
+   header["CDELT2"].set(1.);
    header["CTYPE2"].set("DEC--CAR");
 
    int nee = m_energies.size();
