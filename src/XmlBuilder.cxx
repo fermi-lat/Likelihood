6cfb2b7826f8d411b5a579f6b2e54343be3c977c
jchiang <>
2005-11-16 20:00:33 +0000

    use single XmlParser instance

--- src/XmlBuilder.cxx
+++ src/XmlBuilder.cxx
@@ -3,7 +3,7 @@
  * @brief Concrete implementation that is shareable by subclasses.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/XmlBuilder.cxx,v 1.3 2004/11/11 20:23:11 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/XmlBuilder.cxx,v 1.4 2005/01/03 23:01:21 jchiang Exp $
  */
 
 #include "xmlBase/XmlParser.h"
@@ -12,16 +12,17 @@
 
 #include "Likelihood/XmlBuilder.h"
 
+#include "XmlParser.h"
+
 namespace Likelihood {
 
 XmlBuilder::XmlBuilder() {
-   m_parser = new xmlBase::XmlParser();
+   m_parser = XmlParser::instance();
    m_doc = optimizers::Dom::createDocument();
 }
 
 XmlBuilder::~XmlBuilder() {
    m_doc->release();
-   delete m_parser;
 }
 
 } //namespace Likelihood
---
+++ src/XmlParser.cxx
@@ -0,0 +1,15 @@
+/**
+ * @file XmlParser.cxx
+ * @brief Declaration of XmlParser::s_instance
+ * @author J. Chiang
+ *
+ * $Header$
+ */
+
+#include "XmlParser.h"
+
+namespace Likelihood {
+
+xmlBase::XmlParser * XmlParser::s_instance(0);
+
+} // namespace Likelihood
