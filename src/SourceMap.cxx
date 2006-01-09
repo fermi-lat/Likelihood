fb902ed2043b83ad178bd0280b78ae379ad40ceb
jchiang <>
2006-01-09 20:15:58 +0000

    tagging v11r0p6

--- src/SourceMap.cxx
+++ src/SourceMap.cxx
@@ -4,7 +4,7 @@
  *        response.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SourceMap.cxx,v 1.49 2005/12/21 15:24:51 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SourceMap.cxx,v 1.50 2006/01/09 06:43:28 jchiang Exp $
  */
 
 #include <algorithm>
@@ -113,7 +113,8 @@ SourceMap::SourceMap(Source * src, const CountsMap * dataMap,
       unsigned int indx(0);
       for (int k = 0; energy != energies.end(); ++energy, k++) {
          WcsMap diffuseMap(*diffuseSrc, map_center.ra(), map_center.dec(),
-                           radius, mapsize, *energy, dataMap->proj_name());
+                           radius, mapsize, *energy, dataMap->proj_name(),
+                           dataMap->projection().isGalactic());
          WcsMap convolvedMap = diffuseMap.convolve(*energy, *s_meanPsf, 
                                                    *s_binnedExposure);
          for (pixel = pixels.begin(); pixel != pixels.end(); ++pixel, indx++) {
