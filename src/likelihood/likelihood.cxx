38e3ba00e0aa7279d619a50bfce8efe16f886892
jchiang <>
2006-01-15 16:01:38 +0000

    enable new Ts approx for binned analysis

--- src/likelihood/likelihood.cxx
+++ src/likelihood/likelihood.cxx
@@ -3,7 +3,7 @@
  * @brief Prototype standalone application for the Likelihood tool.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.95 2005/11/16 03:08:18 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.96 2006/01/15 01:57:03 jchiang Exp $
  */
 
 #ifdef TRAP_FPE
@@ -106,7 +106,7 @@ using namespace Likelihood;
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.95 2005/11/16 03:08:18 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.96 2006/01/15 01:57:03 jchiang Exp $
  */
 
 class likelihood : public st_app::StApp {
@@ -168,7 +168,7 @@ likelihood::likelihood()
    : st_app::StApp(), m_helper(0), 
      m_pars(st_app::StApp::getParGroup("gtlikelihood")),
      m_logLike(0), m_opt(0), m_dataMap(0), m_cpuStart(std::clock()),
-     m_tsSrc(0), m_maxdist(10.) {
+     m_tsSrc(0), m_maxdist(20.) {
 }
 
 void likelihood::run() {
@@ -670,6 +670,13 @@ void likelihood::renormModel() {
 
 double likelihood::observedCounts() {
    if (m_statistic == "Binned") {
+      BinnedLikelihood * logLike = dynamic_cast<BinnedLikelihood *>(m_logLike);
+      const std::vector<double> & counts(logLike->countsSpectrum());
+      double totalCounts(0);
+      for (size_t i = 0; i < counts.size(); i++) {
+         totalCounts += counts.at(i);
+      }
+      return totalCounts;
    }
    return m_helper->observation().eventCont().events().size();
 }
