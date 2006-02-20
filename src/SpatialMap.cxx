109e7697ddb8e037685459832f4e7ae30034db6f
jchiang <>
2006-02-20 23:23:02 +0000

    more reliable tests for model map existence; update gtbackfile for single class irfs

--- src/SpatialMap.cxx
+++ src/SpatialMap.cxx
@@ -5,11 +5,15 @@
  * 
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SpatialMap.cxx,v 1.17 2005/10/04 05:38:08 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/SpatialMap.cxx,v 1.18 2005/10/05 00:59:38 jchiang Exp $
  *
  */
 
+#include <stdexcept>
+
 #include "facilities/Util.h"
+
+#include "st_facilities/Util.h"
 
 #include "Likelihood/SkyDirArg.h"
 #include "Likelihood/SpatialMap.h"
@@ -70,6 +74,13 @@ void SpatialMap::readFitsFile(const std::string & fitsFile,
 
    facilities::Util::expandEnvVar(&m_fitsFile);
 
+   if (!st_facilities::Util::fileExists(m_fitsFile)) {
+// The following to stdout is necessary since Xerces seems to corrupt
+// the exception handling when this method is called from
+// SourceFactory::readXml and the program simply aborts.
+      std::cout << "File not found: " << m_fitsFile << std::endl;
+      throw std::runtime_error("File not found: " + m_fitsFile);
+   }
    m_wcsmap = new WcsMap(m_fitsFile, extension);
 }
 
