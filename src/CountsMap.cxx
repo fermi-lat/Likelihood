6a2ca7e3f7388ae3f075c69c00eae8c958f8bdeb
jchiang <>
2006-09-26 20:14:38 +0000

    allow for custom energy bins

--- src/CountsMap.cxx
+++ src/CountsMap.cxx
@@ -1,7 +1,7 @@
 /**
  * @file CountsMap.cxx
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/CountsMap.cxx,v 1.38 2006/06/07 18:22:54 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/CountsMap.cxx,v 1.39 2006/09/18 16:51:13 jchiang Exp $
  */
 
 #include <algorithm>
@@ -152,6 +152,48 @@ CountsMap::CountsMap(const std::string & event_file,
    deleteBinners(binners);
 }
 
+CountsMap::CountsMap(const std::string & event_file, 
+                     const std::string & ev_table, 
+                     const std::string & sc_file, 
+                     const std::string & sc_table, 
+                     double ref_ra, double ref_dec, const std::string & proj, 
+                     unsigned long num_x_pix, unsigned long num_y_pix, 
+                     double x_pix_scale, double y_pix_scale,
+                     double axis_rot, bool use_lb,
+                     const std::string & ra_field, 
+                     const std::string & dec_field, 
+                     const std::vector<double> & emins,
+                     const std::vector<double> & emaxs) 
+   : DataProduct(event_file, ev_table, evtbin::Gti(event_file)), 
+     m_hist(0), m_proj_name(proj), 
+     m_crpix(), m_crval(), m_cdelt(), m_axis_rot(axis_rot), 
+     m_use_lb(use_lb), m_proj(0) {
+
+   std::vector<evtbin::Binner *> binners;
+
+// The astro::SkyDir::project method will convert ra and dec into 
+// bin indices, so we set up the LinearBinners generically by index.
+   binners.push_back(new evtbin::LinearBinner(0.5, num_x_pix + 0.5, 1., 
+                                              ra_field));
+   binners.push_back(new evtbin::LinearBinner(0.5, num_y_pix + 0.5, 1., 
+                                              dec_field));
+
+// Use custom energy bins.
+   std::vector<evtbin::Binner::Interval> energy_intervals;
+   std::vector<double>::const_iterator emin(emins.begin());
+   std::vector<double>::const_iterator emax(emaxs.begin());
+   for ( ; emin != emins.end() && emax != emaxs.end(); ++emin, ++emax) {
+      energy_intervals.push_back(evtbin::Binner::Interval(*emin, *emax));
+   }
+   binners.push_back(new evtbin::OrderedBinner(energy_intervals,
+                                               "photon energy"));
+   init(binners, event_file, ev_table, sc_file, sc_table, 
+        num_x_pix, num_y_pix, ref_ra, ref_dec, x_pix_scale, y_pix_scale,
+        emins.front(), emaxs.back(), emins.size(), use_lb, proj);
+
+   deleteBinners(binners);
+}
+
 CountsMap::CountsMap(const std::string & countsMapFile) 
    : DataProduct(countsMapFile, "", evtbin::Gti(countsMapFile)),
      m_use_lb(false) {
@@ -262,6 +304,7 @@ void CountsMap::init(std::vector<evtbin::Binner *> & binners,
    m_crval[2] = emin;
    m_cdelt[0] = -x_pix_scale;
    m_cdelt[1] = y_pix_scale;
+// This may not be correct if custom energy bins are used.
    m_cdelt[2] = log(emax/emin)/(nenergies-1.);
    
    m_proj = new astro::SkyProj(proj, m_crpix, m_crval, m_cdelt, 
