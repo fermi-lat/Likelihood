1b56265479e0100a44f4bb22fb3cd05483a96ef7
golpa <>
2007-12-10 19:59:34 +0000

    Added mython module used by SCons during library dependency computation

---
+++ LikelihoodLib.py
@@ -0,0 +1,19 @@
+def generate(env, **kw):
+    env.Tool('addLibrary', library=['Likelihood'], package = 'Likelihood')
+    env.Tool('astroLib')
+    env.Tool('xmlBaseLib')
+    env.Tool('tipLib')
+    env.Tool('evtbinLib')
+    env.Tool('map_toolsLib')
+    env.Tool('optimizersLib')
+    env.Tool('irfLoaderLib')
+    env.Tool('st_facilitiesLib')
+    env.Tool('dataSubselectorLib')
+    env.Tool('hoopsLib')
+    env.Tool('st_appLib')
+    env.Tool('st_graphLib')
+    env.Tool('addLibrary', library=env['cfitsioLibs'])
+    env.Tool('addLibrary', library=env['fftwLibs'])
+    
+def exists(env):
+    return 1
