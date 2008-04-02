f50a3e45ee41bb376969797583f2bdec77bec812
jchiang <>
2008-04-02 14:52:56 +0000

    use wcslib (and FITS template) for TS maps

--- src/TsMap/TsMap.cxx
+++ src/TsMap/TsMap.cxx
@@ -1,10 +1,10 @@
 /**
  * @file TsMap.cxx
- * @brief Prototype standalone application for producing
- * "test-statistic" maps.
+ * @brief Application for producing "test-statistic" maps.
+ *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/TsMap/TsMap.cxx,v 1.38 2007/07/03 22:48:19 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/TsMap/TsMap.cxx,v 1.39 2007/07/11 21:21:12 jchiang Exp $
  */
 
 #include <cmath>
@@ -13,7 +13,11 @@
 
 #include <sstream>
 
-#include "fitsio.h"
+#include "facilities/commonUtilities.h"
+
+#include "tip/Header.h"
+#include "tip/IFileSvc.h"
+#include "tip/Image.h"
 
 #include "st_stream/StreamFormatter.h"
 
@@ -38,10 +42,8 @@ using namespace Likelihood;
  * @brief Class for encapsulating methods for creating a test-statistic
  * map.
  *
- * @author J. Chiang
- *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/TsMap/TsMap.cxx,v 1.38 2007/07/03 22:48:19 jchiang Exp $
  */
+
 class TsMap : public st_app::StApp {
 public:
    TsMap();
@@ -64,23 +66,18 @@ private:
    st_stream::StreamFormatter * m_formatter;
 
    std::vector<std::string> m_eventFiles;
-   std::vector<double> m_lonValues;
-   std::vector<double> m_latValues;
-   std::vector< std::vector<double> > m_tsMap;
+   std::vector<astro::SkyDir> m_dirs;
+   std::vector<float> m_tsMap;
    std::string m_coordSys;
+   std::vector<double> m_crpix;
+   std::vector<double> m_crval;
+   std::vector<double> m_cdelt;
    void readSrcModel();
    void readEventData();
    void selectOptimizer();
    void setGrid();
    void computeMap();
-   void writeFitsFile(const std::string &filename,
-                      std::vector<double> &lon, 
-                      std::vector<double> &lat,
-                      std::vector< std::vector<double> > &map,
-                      const std::string &coordSystem);
-   void fitsReportError(FILE *stream, int status);
-   void makeDoubleVector(double xmin, double xmax, int nx,
-                         std::vector<double> &xVals);
+   void writeFitsFile();
    void setPointSourceSpectrum(PointSource &src);
 
    static std::string s_cvs_id;
@@ -88,15 +85,15 @@ private:
 
 st_app::StAppFactory<TsMap> myAppFactory("gttsmap");
 
-TsMap::TsMap() : st_app::StApp(), m_helper(0), 
-                 m_pars(st_app::StApp::getParGroup("gttsmap")),
-                 m_logLike(0), m_opt(0),
-                 m_formatter(new st_stream::StreamFormatter("gttsmap", "", 2)) 
-{
+TsMap::TsMap() 
+   : st_app::StApp(), m_helper(0), 
+     m_pars(st_app::StApp::getParGroup("gttsmap")),
+     m_logLike(0), m_opt(0),
+     m_formatter(new st_stream::StreamFormatter("gttsmap", "", 2)) {
    setVersion(s_cvs_id);
 }
 
-std::string TsMap::s_cvs_id("$Name:  $");
+std::string TsMap::s_cvs_id("$Name: v13r6p3 $");
 
 void TsMap::banner() const {
    int verbosity = m_pars["chatter"];
@@ -120,17 +117,17 @@ void TsMap::run() {
    st_facilities::Util::file_ok(m_pars["evfile"]);
    st_facilities::Util::resolve_fits_files(m_pars["evfile"], m_eventFiles);
    std::string ev_table = m_pars["evtable"];
-   bool compareGtis(false);
-   bool relyOnStreams(false);
+   bool compareGtis;
+   bool relyOnStreams;
    std::string respfunc = m_pars["irfs"];
    bool skipEventClassCuts(respfunc != "DSS");
-   for (unsigned int i = 1; i < m_eventFiles.size(); i++) {
+   for (size_t i(1); i < m_eventFiles.size(); i++) {
       AppHelpers::checkCuts(m_eventFiles[0], ev_table,
                             m_eventFiles[i], ev_table,
-                            compareGtis, relyOnStreams, skipEventClassCuts);
+                            compareGtis=false, 
+                            relyOnStreams=false, 
+                            skipEventClassCuts);
    }
-/// @bug Ascertain why this was called with just the first event file.
-///   m_helper->setRoi(m_eventFiles[0]);
    m_helper->setRoi();
    m_helper->readScData();
    m_helper->readExposureMap();
@@ -143,15 +140,14 @@ void TsMap::run() {
    setGrid();
 
    computeMap();
-   writeFitsFile(m_pars["outfile"], m_lonValues, m_latValues,
-                 m_tsMap, m_coordSys);
+   writeFitsFile();
 }
 
 void TsMap::readEventData() {
-   std::vector<std::string>::const_iterator evIt = m_eventFiles.begin();
-   for ( ; evIt != m_eventFiles.end(); evIt++) {
-      st_facilities::Util::file_ok(*evIt);
-      m_helper->observation().eventCont().getEvents(*evIt);
+   std::vector<std::string>::const_iterator evFile(m_eventFiles.begin());
+   for ( ; evFile != m_eventFiles.end(); ++evFile) {
+      st_facilities::Util::file_ok(*evFile);
+      m_helper->observation().eventCont().getEvents(*evFile);
    }
 }
 
@@ -171,26 +167,33 @@ void TsMap::selectOptimizer() {
 }
 
 void TsMap::setGrid() {
-   int nlon = m_pars["nx"];
-   int nlat = m_pars["ny"];
-   makeDoubleVector(m_pars["xref_min"], m_pars["xref_max"], nlon, m_lonValues);
-   makeDoubleVector(m_pars["yref_min"], m_pars["yref_max"], nlat, m_latValues);
-   m_tsMap.resize(nlat);
-   for (int i = 0; i < nlat; i++) {
-      m_tsMap.reserve(nlon);
+   int nxpix = m_pars["nxpix"];
+   int nypix = m_pars["nypix"];
+   double xref = m_pars["xref"];
+   double yref = m_pars["yref"];
+   double binsize = m_pars["binsz"];
+   bool is_galactic(m_pars["coordsys"] == "GAL");
+   std::string proj_name = m_pars["proj"];
+
+   double crpix[] = {nxpix/2. + 0.5, nypix/2. + 0.5};
+   m_crpix = std::vector<double>(crpix, crpix+2);
+   double crval[] = {xref, yref};
+   m_crval = std::vector<double>(crval, crval+2);
+   double cdelt[] = {binsize, binsize};
+   m_cdelt = std::vector<double>(cdelt, cdelt+2);
+   astro::SkyProj proj(proj_name, crpix, crval, cdelt, 0, is_galactic);
+
+   m_dirs.clear();
+   for (int j(0); j < nypix; j++) {
+      for (int i(0); i < nxpix; i++) {
+         m_dirs.push_back(astro::SkyDir(i+1, j+1, proj));
+      }
    }
 }
 
 void TsMap::computeMap() {
-   double ra(m_lonValues.front());
-   double dec(m_latValues.front());
-   std::string coordSys = m_pars["coordsys"];
-   if (coordSys == "GAL") {
-      astro::SkyDir my_dir(ra, dec, astro::SkyDir::GALACTIC);
-      ra = my_dir.ra();
-      dec = my_dir.dec();
-   }
-   PointSource testSrc(ra, dec, m_helper->observation());
+   Likelihood::PointSource testSrc(m_dirs.at(0).ra(), m_dirs.at(0).dec(), 
+                                   m_helper->observation());
    setPointSourceSpectrum(testSrc);
    testSrc.setName("testSource");
 
@@ -206,49 +209,28 @@ void TsMap::computeMap() {
    }
    bool computeExposure(true);
 
-   for (unsigned int jj = 0; jj < m_latValues.size(); jj++) {
-      if ((jj % m_latValues.size()/20) == 0) {
+   for (size_t i(0); i < m_dirs.size(); i++) {
+      if ((i % (m_dirs.size()/20)) == 0) {
          m_formatter->warn() << ".";
       }
-      for (unsigned int ii = 0; ii < m_lonValues.size(); ii++) {
-         if (m_coordSys == "CEL") {
-            testSrc.setDir(m_lonValues[ii], m_latValues[jj], 
-                           computeExposure, false);
-         } else if (m_coordSys == "GAL") {
-            testSrc.setGalDir(m_lonValues[ii], m_latValues[jj], 
-                              computeExposure, false);
-         } else {
-            throw std::invalid_argument("Invalid coordinate system: "
-                                        + m_coordSys);
-         }
-         m_logLike->addSource(&testSrc);
-         try {
-            m_opt->find_min(verbosity, tol);
-            m_tsMap[jj].push_back(2.*(m_logLike->value() - logLike0));
-         } catch (optimizers::Exception &eObj) {
-            m_formatter->err() << eObj.what() << std::endl;
-            // Default null value.
-            m_tsMap[jj].push_back(0);
-         }
-         m_formatter->info(3) << m_lonValues[ii] << "  "
-                              << m_latValues[jj] << "  "
-                              << m_tsMap[jj][ii] << "  "
-                              << m_helper->observation().eventCont().events().size() 
-                              << std::endl;
-         m_logLike->deleteSource(testSrc.getName());
+      testSrc.setDir(m_dirs.at(i).ra(), m_dirs.at(i).dec(),
+                     computeExposure, false);
+
+      m_logLike->addSource(&testSrc);
+      try {
+         m_opt->find_min(verbosity, tol);
+         m_tsMap.push_back(2.*(m_logLike->value() - logLike0));
+      } catch (optimizers::Exception & eObj) {
+         m_formatter->err() << eObj.what() << std::endl;
+         // Default null value.
+         m_tsMap.push_back(0);
       }
+      m_formatter->info(3) << m_dirs.at(i).ra() << "  "
+                           << m_dirs.at(i).dec() << "  "
+                           << m_tsMap.back() << std::endl;
+      m_logLike->deleteSource(testSrc.getName());
    }
    m_formatter->warn() << "!" << std::endl;
-}
-
-void TsMap::makeDoubleVector(double xmin, double xmax, int nx,
-                             std::vector<double> &xVals) {
-   xVals.clear();
-   xVals.reserve(nx);
-   double xstep = (xmax - xmin)/(nx-1);
-   for (int i = 0; i < nx; i++) {
-      xVals.push_back(xstep*i + xmin);
-   }
 }
 
 void TsMap::setPointSourceSpectrum(PointSource &src) {
@@ -266,99 +248,55 @@ void TsMap::setPointSourceSpectrum(PointSource &src) {
    src.setSpectrum(pl);
 }
 
-void TsMap::writeFitsFile(const std::string &filename, 
-                          std::vector<double> &lon,
-                          std::vector<double> &lat,
-                          std::vector< std::vector<double> > &map,
-                          const std::string &coordSystem) {
-   fitsfile *fptr;
-   int status = 0;
-   
-// Always overwrite an existing file.
-   remove(filename.c_str());
-   fits_create_file(&fptr, filename.c_str(), &status);
-   fitsReportError(stderr, status);
-
-   int bitpix = DOUBLE_IMG;
-   long naxis = 2;
-   long naxes[] = {lon.size(), lat.size()};
-   fits_create_img(fptr, bitpix, naxis, naxes, &status);
-   fitsReportError(stderr, status);
-
-// Repack exposure into a 1D vector
-   std::vector<double> mapVector;
-   mapVector.reserve(lon.size()*lat.size());
-   for (unsigned int i = 0; i < lon.size(); i++) {
-      for (unsigned int j = 0; j < lat.size(); j++) {
-         mapVector.push_back(map[i][j]);
-      }
+void TsMap::writeFitsFile() {
+   std::string outfile = m_pars["outfile"];
+   if (st_facilities::Util::fileExists(outfile)) {
+      std::remove(outfile.c_str());
    }
+   std::string dataPath = 
+      facilities::commonUtilities::getDataPath("Likelihood");
+   std::string templateFile = 
+      facilities::commonUtilities::joinPath(dataPath, "TsMapTemplate");
 
-// Write the exposure map data.
-   long group = 0;
-   long firstelem = 1;
-   long nelements = lon.size()*lat.size();
-   fits_write_img_dbl(fptr, group, firstelem, nelements,
-                      &mapVector[0], &status);
-   fitsReportError(stderr, status);
+   tip::IFileSvc & fileSvc(tip::IFileSvc::instance());
+   fileSvc.createFile(outfile, templateFile);
 
-// Write some keywords.
-   double l0 = lon[0];
-   fits_update_key(fptr, TDOUBLE, "CRVAL1", &l0, 
-                   "longitude of reference pixel", &status);
-   fitsReportError(stderr, status);
-   double b0 = lat[0];
-   fits_update_key(fptr, TDOUBLE, "CRVAL2", &b0, 
-                   "latitude of reference pixel", &status);
-   fitsReportError(stderr, status);
-   
-   double lstep = lon[1] - lon[0];
-   fits_update_key(fptr, TDOUBLE, "CDELT1", &lstep, 
-                   "longitude step at ref. pixel", &status);
-   fitsReportError(stderr, status);
-   double bstep = lat[1] - lat[0];
-   fits_update_key(fptr, TDOUBLE, "CDELT2", &bstep, 
-                   "latitude step at ref. pixel", &status);
-   fitsReportError(stderr, status);
-   
-   float crpix1 = 1.0;
-   fits_update_key(fptr, TFLOAT, "CRPIX1", &crpix1, 
-                   "reference pixel for longitude coordinate", &status);
-   fitsReportError(stderr, status);
-   float crpix2 = 1.0;
-   fits_update_key(fptr, TFLOAT, "CRPIX2", &crpix2, 
-                   "reference pixel for latitude coordinate", &status);
-   fitsReportError(stderr, status);
-   
-   if (coordSystem == "CEL") {
-      char *ctype1 = "RA---CAR";
-      fits_update_key(fptr, TSTRING, "CTYPE1", ctype1, 
-                      "right ascension", &status);
-      fitsReportError(stderr, status);
-      char *ctype2 = "DEC--CAR";
-      fits_update_key(fptr, TSTRING, "CTYPE2", ctype2, 
-                      "declination", &status);
-      fitsReportError(stderr, status);
-   } else if (coordSystem == "GAL") {
-      char *ctype1 = "GLON-CAR";
-      fits_update_key(fptr, TSTRING, "CTYPE1", ctype1, 
-                      "Galactic longitude", &status);
-      fitsReportError(stderr, status);
-      char *ctype2 = "GLAT-CAR";
-      fits_update_key(fptr, TSTRING, "CTYPE2", ctype2, 
-                      "Galactic latitude", &status);
-      fitsReportError(stderr, status);
+   tip::Image * image(fileSvc.editImage(outfile, ""));
+
+   std::vector<long> naxes;
+   naxes.push_back(m_pars["nxpix"]);
+   naxes.push_back(m_pars["nypix"]);
+   image->setImageDimensions(naxes);
+
+   tip::Header & header(image->getHeader());
+   header["TELESCOP"].set("GLAST");
+   header["INSTRUME"].set("LAT SIMULATION");
+
+   header["DATE-OBS"].set("");
+   header["DATE-END"].set("");
+
+
+   header["CRVAL1"].set(m_crval.at(0));
+   header["CRVAL2"].set(m_crval.at(1));
+
+   header["CRPIX1"].set(m_crpix.at(0));
+   header["CRPIX2"].set(m_crpix.at(1));
+
+   header["CDELT1"].set(m_cdelt.at(0));
+   header["CDELT2"].set(m_cdelt.at(1));
+
+   header["CROTA"].set(0);
+
+   header["CREATOR"].set("gttsmap");
+
+   std::string proj = m_pars["proj"];
+   if (m_pars["coordsys"] == "GAL") {
+      header["CTYPE1"].set("GLON-" + proj);
+      header["CTYPE2"].set("GLAT-" + proj);
+   } else {
+      header["CTYPE1"].set("RA---" + proj);
+      header["CTYPE2"].set("DEC--" + proj);
    }
-   
-   fits_close_file(fptr, &status);
-   fitsReportError(stderr, status);
-   
-   return;
-}
-
-void TsMap::fitsReportError(FILE *stream, int status) {
-   fits_report_error(stream, status);
-   if (status != 0) {
-      throw std::runtime_error("TsMap::writeFitsFile: cfitsio error.");
-   }
+   image->set(m_tsMap);
+   delete image;
 }
