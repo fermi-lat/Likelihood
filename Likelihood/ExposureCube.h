3b696a3c599296381a8f1e3372c4025b01671103
jchiang <>
2009-04-06 23:01:41 +0000

    Use wrapper class to convert phi to degrees when calling map_tools::Exposure::integral

--- Likelihood/ExposureCube.h
+++ Likelihood/ExposureCube.h
@@ -3,7 +3,7 @@
  * @brief Exposure time hypercube.
  * @author J. Chiang <jchiang@slacs.stanford.edu>
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/ExposureCube.h,v 1.6 2009/03/16 20:44:57 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/ExposureCube.h,v 1.7 2009/03/16 23:19:06 jchiang Exp $
  */
 
 #ifndef Likelihood_ExposureCube_h
@@ -24,7 +24,7 @@ namespace Likelihood {
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/ExposureCube.h,v 1.6 2009/03/16 20:44:57 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/ExposureCube.h,v 1.7 2009/03/16 23:19:06 jchiang Exp $
  */
 
 class ExposureCube {
@@ -49,7 +49,8 @@ public:
    template<class T>
    double value(const astro::SkyDir & dir, const T & aeff) const {
       if (m_hasPhiDependence) {
-         return m_exposure->integral(dir, aeff);
+         AeffWrapper<T> myAeff(aeff);
+         return m_exposure->integral(dir, myAeff);
       }
       return (*m_exposure)(dir, aeff);
    }
@@ -73,6 +74,21 @@ private:
    bool m_hasPhiDependence;
 
    bool phiDependence(const std::string & filename) const;
+
+   // healpix::CosineBinner::integral assumes that phi is in radians instead of
+   // degrees, contrary to established conventions.  This class wraps the 
+   // integral(costh, phi) method to do the conversion.
+   template <class Aeff>
+   class AeffWrapper {
+   public:
+      AeffWrapper::AeffWrapper(const Aeff & aeff) : m_aeff(aeff) {}
+      double integral(double costh, double phi) const {
+         phi *= 180./M_PI;
+         return m_aeff.integral(costh, phi);
+      }
+   private:
+      const Aeff & m_aeff;
+   };
 
 };
 
