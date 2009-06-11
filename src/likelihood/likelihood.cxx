86af6471861dddc93214e7d27e3cd4f04f0c0a13
jchiang <>
2009-06-11 21:46:49 +0000

    remove fluxmdl option

--- src/likelihood/likelihood.cxx
+++ src/likelihood/likelihood.cxx
@@ -3,7 +3,7 @@
  * @brief Prototype standalone application for the Likelihood tool.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.144 2009/04/09 18:18:59 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.145 2009/05/15 04:12:21 jchiang Exp $
  */
 
 #ifdef TRAP_FPE
@@ -88,7 +88,7 @@ using namespace Likelihood;
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.144 2009/04/09 18:18:59 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/likelihood/likelihood.cxx,v 1.145 2009/05/15 04:12:21 jchiang Exp $
  */
 
 class likelihood : public st_app::StApp {
@@ -136,7 +136,7 @@ private:
    void readSourceModel();
    void selectOptimizer(std::string optimizer="");
    void writeSourceXml();
-   void writeFluxXml();
+//   void writeFluxXml();
    void writeCountsSpectra();
    void plotCountsSpectra();
    void writeCountsMap();
@@ -284,7 +284,7 @@ void likelihood::run() {
          plotCountsSpectra();
       }
    } while (queryLoop && prompt("Refit? [y] "));
-   writeFluxXml();
+//   writeFluxXml();
    if (m_pars["save"]) {
       writeCountsSpectra();
    }
@@ -323,9 +323,9 @@ void likelihood::promptForParameters() {
    m_pars.Prompt("sfile");
    AppHelpers::checkOutputFile(m_pars["clobber"], 
                                m_pars["sfile"]);
-   m_pars.Prompt("fluxmdl");
-   AppHelpers::checkOutputFile(m_pars["clobber"], 
-                               m_pars["fluxmdl"]);
+//    m_pars.Prompt("fluxmdl");
+//    AppHelpers::checkOutputFile(m_pars["clobber"], 
+//                                m_pars["fluxmdl"]);
    m_pars.Prompt("irfs");
    m_pars.Prompt("edisp");
    m_pars.Prompt("optimizer");
@@ -425,14 +425,14 @@ void likelihood::writeSourceXml() {
    }
 }
 
-void likelihood::writeFluxXml() {
-   std::string xml_fluxFile = m_pars["fluxmdl"];
-   if (xml_fluxFile != "none") {
-      m_formatter->info() << "Writing flux-style xml model file to "
-                          << xml_fluxFile << std::endl;
-      m_logLike->write_fluxXml(xml_fluxFile);
-   }
-}
+// void likelihood::writeFluxXml() {
+//    std::string xml_fluxFile = m_pars["fluxmdl"];
+//    if (xml_fluxFile != "none") {
+//       m_formatter->info() << "Writing flux-style xml model file to "
+//                           << xml_fluxFile << std::endl;
+//       m_logLike->write_fluxXml(xml_fluxFile);
+//    }
+// }
 
 class EventData {
 public:
