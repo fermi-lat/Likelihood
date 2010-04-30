a122c04e97523225fbf5c37343f44282a12f88b6
jchiang <>
2010-04-30 22:18:12 +0000

    tagging v16r4p4

--- Likelihood/Source.h
+++ Likelihood/Source.h
@@ -4,7 +4,7 @@
  *
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/Source.h,v 1.44 2010/03/03 18:09:09 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/Source.h,v 1.45 2010/03/07 18:23:57 jchiang Exp $
  */
 
 #ifndef Likelihood_Source_h
@@ -34,7 +34,7 @@ namespace Likelihood {
  *
  * @author J. Chiang
  *    
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/Source.h,v 1.44 2010/03/03 18:09:09 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/Source.h,v 1.45 2010/03/07 18:23:57 jchiang Exp $
  */
 
 class Source {
@@ -101,6 +101,9 @@ public:
    virtual void setName(const std::string & name) {
       m_name = name;
    }
+
+   /// @return true if all spectral parameters are fixed.
+   bool fixedSpectrum() const;
 
    /// Access a unique source identifier.
    virtual const std::string & getName() const {
