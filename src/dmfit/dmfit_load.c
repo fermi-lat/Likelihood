6361c61356179b2f81241a2b9bbb10ccbd6bf2ec
cohen <>
2012-11-21 01:58:52 +0000

    new version of dmfit

--- src/dmfit/dmfit_load.c
+++ src/dmfit/dmfit_load.c
@@ -1,14 +1,21 @@
-/* dmfit_load.f -- translated by f2c (version 19991025).
-   You must link the resulting object file with the libraries:
-	-lf2c -lm   (in that order)
+/* dmfit_load.f -- translated by f2c (version 20090411).
+   You must link the resulting object file with libf2c:
+	on Microsoft Windows system, link with libf2c.lib;
+	on Linux or Unix systems, link with .../path/to/libf2c.a -lm
+	or, if you install libf2c.a in a standard place, with -lf2c -lm
+	-- in that order, at the end of the command line, as in
+		cc *.o -lf2c -lm
+	Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
+
+		http://www.netlib.org/f2c/libf2c.zip
 */
 
 #include "f2c/f2c.h"
 
 /* Common Block Declarations */
 
-extern struct {
-    real phidif[45360]	/* was [252][18][8] */;
+struct {
+    real phidif[72576]	/* was [252][24][12] */;
     integer hasmooth;
 } hasim_;
 
@@ -16,30 +23,26 @@ extern struct {
 
 /* Table of constant values */
 
+static integer c__4 = 4;
 static integer c__1 = 1;
 
-integer dmfit_load__(filename, filename_len)
-char *filename;
-ftnlen filename_len;
+/* Subroutine */ int dmfit_load__(char *filename, ftnlen filename_len)
 {
-    /* Format strings */
-    static char fmt_2000[] = "(1000(1x,e12.6))";
-
     /* System generated locals */
-    integer ret_val, i__1;
+    integer i__1;
     olist o__1;
     cllist cl__1;
 
     /* Builtin functions */
-    integer f_open(), s_rsfe(), do_fio(), e_rsfe(), f_clos();
+    integer f_open(olist *), s_rsle(cilist *), do_lio(integer *, integer *, 
+	    char *, ftnlen), e_rsle(void), f_clos(cllist *);
 
     /* Local variables */
-    static integer j, k, l;
-    static doublereal milow[8];
-    static integer zn;
+    static integer j, k, l, zn;
+    static doublereal milow[12];
 
     /* Fortran I/O blocks */
-    static cilist io___6 = { 0, 13, 0, fmt_2000, 0 };
+    static cilist io___6 = { 0, 13, 0, 0, 0 };
 
 
 /* -----data tables */
@@ -50,23 +53,31 @@ ftnlen filename_len;
 /* c c-bar */
     milow[1] = 1.;
 /* b b-bar */
-    milow[2] = 8.;
+    milow[2] = 1.;
 /* t t-bar */
     milow[3] = 1.;
 /* tau+ tau- */
-    milow[4] = 4.;
+    milow[4] = 1.;
 /* w+ w- */
-    milow[5] = 5.;
+    milow[5] = 1.;
 /* z z */
     milow[6] = 1.;
 /* mu+ mu- */
     milow[7] = 1.;
+/* gluons */
+    milow[8] = 1.;
+/* gluons */
+    milow[9] = 1.;
+/* gluons */
+    milow[10] = 1.;
+/* gluons */
+    milow[11] = 1.;
 /* -----clear the tables */
 /* gluons */
-    for (j = 1; j <= 8; ++j) {
-	for (k = 1; k <= 18; ++k) {
+    for (j = 1; j <= 12; ++j) {
+	for (k = 1; k <= 24; ++k) {
 	    for (l = 0; l <= 250; ++l) {
-		hasim_1.phidif[l + (k + j * 18) * 252 - 4787] = (float)0.;
+		hasim_1.phidif[l + (k + j * 24) * 252 - 6299] = 0.f;
 	    }
 	}
     }
@@ -82,16 +93,16 @@ ftnlen filename_len;
     o__1.ofm = "formatted";
     o__1.oblnk = 0;
     f_open(&o__1);
-    for (j = 1; j <= 8; ++j) {
-	for (k = 1; k <= 18; ++k) {
+    for (j = 1; j <= 12; ++j) {
+	for (k = 1; k <= 24; ++k) {
 	    if ((doublereal) k >= milow[j - 1]) {
-		s_rsfe(&io___6);
+		s_rsle(&io___6);
 		i__1 = zn - 1;
 		for (l = 0; l <= i__1; ++l) {
-		    do_fio(&c__1, (char *)&hasim_1.phidif[l + (k + j * 18) * 
-			    252 - 4787], (ftnlen)sizeof(real));
+		    do_lio(&c__4, &c__1, (char *)&hasim_1.phidif[l + (k + j * 
+			    24) * 252 - 6299], (ftnlen)sizeof(real));
 		}
-		e_rsfe();
+		e_rsle();
 	    }
 	}
     }
@@ -99,21 +110,24 @@ ftnlen filename_len;
     cl__1.cunit = 13;
     cl__1.csta = 0;
     f_clos(&cl__1);
-    for (j = 1; j <= 8; ++j) {
-	for (k = 1; k <= 18; ++k) {
+    for (j = 1; j <= 12; ++j) {
+	for (k = 1; k <= 24; ++k) {
 	    i__1 = zn - 1;
 	    for (l = 0; l <= i__1; ++l) {
 /* correct units of dyield / dz */
-		hasim_1.phidif[l + (k + j * 18) * 252 - 4787] /= 1. / (
-			doublereal) zn;
+		hasim_1.phidif[l + (k + j * 24) * 252 - 6299] = 
+			hasim_1.phidif[l + (k + j * 24) * 252 - 6299];
+/*     &            (1.0d0/dble(zn)) */
+/* / */
 	    }
-	    hasim_1.phidif[(k + j * 18) * 252 - 4788] = hasim_1.phidif[(k + j 
-		    * 18) * 252 - 4787];
-	    hasim_1.phidif[zn + (k + j * 18) * 252 - 4787] = hasim_1.phidif[
-		    zn - 1 + (k + j * 18) * 252 - 4787];
+	    hasim_1.phidif[(k + j * 24) * 252 - 6300] = hasim_1.phidif[(k + j 
+		    * 24) * 252 - 6299];
+	    hasim_1.phidif[zn + (k + j * 24) * 252 - 6299] = hasim_1.phidif[
+		    zn - 1 + (k + j * 24) * 252 - 6299];
 	}
     }
-    ret_val = 1;
-    return ret_val;
+/*         dmfit_load=1 */
+/* 2000 format(1000(1x,e12.6)) */
+    return 0;
 } /* dmfit_load__ */
 
