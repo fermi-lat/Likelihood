e613c95076f0197ff4ee0ef109b7f2d34cc3c879
echarles <>
2016-01-15 17:30:18 +0000

    Fix bug in building PSF image when using HEALPix projections

--- src/WcsMap2.cxx
+++ src/WcsMap2.cxx
@@ -4,7 +4,7 @@
  * uses WCS projections for indexing its internal representation.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/WcsMap2.cxx,v 1.22 2015/12/10 00:58:00 echarles Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/WcsMap2.cxx,v 1.23 2015/12/10 01:42:17 echarles Exp $
  */
 
 #include <cmath>
@@ -487,7 +487,7 @@ ProjMap* WcsMap2::convolve(double energy, const MeanPsf & psf,
       psf_image.at(j).resize(npix, 0);
       for (int i(0); i < npix; i++) {
          if (my_proj.testpix2sph(i+1, j+1) == 0) {
-            std::pair<double, double> coord = getProj()->pix2sph(i+1, j+1);
+	    std::pair<double, double> coord = my_proj.pix2sph(i+1, j+1);
             astro::SkyDir dir(coord.first, coord.second, 
                               astro::SkyDir::EQUATORIAL);
             double theta = getRefDir().difference(dir)*180./M_PI;
@@ -501,7 +501,7 @@ ProjMap* WcsMap2::convolve(double energy, const MeanPsf & psf,
    check_negative_pixels(counts);
    check_negative_pixels(psf_image);
    my_image->m_image.push_back(Convolve::convolve2d(counts, psf_image));
-  
+
    return my_image;
 }
 
