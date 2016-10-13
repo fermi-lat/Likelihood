96b307c0f1e72e9c60162b2213bb804b6ad87f44
echarles <>
2016-10-13 01:52:53 +0000

    added BinnedCountsCache class

---
+++ Likelihood/BinnedCountsCache.h
@@ -0,0 +1,193 @@
+/**
+ * @file BinnedCountsCache.h
+ * @brief Small class to encapsulate stuff that needed for BinnedLikelihood.
+ * @author J. Chiang
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/Likelihood/BinnedCountsCache.h,v 1.92 2016/09/29 00:30:23 echarles Exp $
+ */
+
+#ifndef Likelihood_BinnedCountsCache_h
+#define Likelihood_BinnedCountsCache_h
+
+#include <string>
+#include <vector>
+
+#include "Likelihood/CountsMapBase.h"
+
+
+
+namespace tip {
+   class Extension;
+}
+
+
+namespace Likelihood {
+
+  class WeightMap;
+  class ProjMap;
+
+  /*
+   * @class BinnedCountsCache
+   * @brief Cache of CountsMap, weights maps and other things we pass around for BinnedLikelihood
+   *
+   */
+  
+  class BinnedCountsCache {
+    
+  public:
+    
+    /* Regular c'tor 
+       
+       dataMap      : Observed data.  Defines the binning used for the analysis.
+       observation  : Wrapper containing information about the observation
+       weightMap    : Map with weights to use for analysis, NULL for no-weights
+       srcMapsFile  : Name of file containing Source Maps (i.e., the output of gtsrcmaps)
+    */	
+    BinnedCountsCache(CountsMapBase & dataMap, 
+		      const Observation & observation,
+		      const ProjMap* weightMap,
+		      const std::string & srcMapsFile);
+    
+    ///
+    virtual ~BinnedCountsCache();
+    
+    /* --------------- Simple Access functions ----------------------*/
+    
+    /// Return the binned observed data
+    inline const CountsMapBase & countsMap() const { return m_dataMap; }
+    
+    /// Return the energy bin edges
+    inline const std::vector<double> & energies() const { return m_dataMap.energies(); }
+
+    /// Return the log of the ratios of the energy bin edges
+    inline const std::vector<double> & log_energy_ratios() const { return m_log_energy_ratios; }
+    
+    /// Return the number of energies
+    inline size_t num_energies() const { return m_dataMap.num_energies(); }
+    
+    /// Return the number of energy bins
+    inline size_t num_ebins() const { return m_dataMap.num_ebins(); }
+
+    /// Return the number of pixels
+    inline size_t num_pixels() const { return m_numPixels; }
+    
+    /// Return the size of the data map
+    inline size_t data_map_size() const { return m_dataMap.data().size(); }
+    
+    /// Return the size of the scours maps
+    inline size_t source_map_size() const { return m_dataMap.num_energies() * m_numPixels; }
+    
+    /// Return the number of filled pixels
+    inline size_t nFilled() const { return m_filledPixels.size(); }
+
+    /// Return the weights in their original projection
+    inline const ProjMap* weightMap_orig() const { return m_weightMap_orig; }
+    
+    /// Return the weights reprojected into counts map binning
+    inline const WeightMap* weightMap() const { return m_weightMap; }
+    
+    /// Return the weighted counts map
+    inline const CountsMapBase* weightedCounts() const { return m_weightedCounts; }
+    
+    /// Return true if the weighted counts map exists
+    inline bool has_weights() const { return m_weightedCounts != 0; }
+
+    /// Get the data
+    inline const std::vector<float>& data(bool weighted = false) const { 
+      return weighted ? m_weightedCounts->data() : m_dataMap.data();
+    }
+
+    /// Return the observed counts spectrum
+    inline const std::vector<double> & countsSpectrum(bool weighted=false) const { 
+      return weighted ? m_countsSpectrum_wt : m_countsSpectrum; 
+    }
+    
+    /// Get the filled pixel indices
+    inline const std::vector<unsigned int> filledPixels() const { return m_filledPixels; }
+
+    /// Set the counts map by hand
+    void setCountsMap(const std::vector<float> & counts);
+
+    /* Save the weights SourceMap to the SourceMap file
+       
+       replace : if true, replace the current version 
+    */
+    tip::Extension* saveWeightsMap(const std::string& srcMapsFile, 
+				   bool replace=false) const;
+    
+    /// Fill the map of weighted counts
+    void fillWeightedCounts();
+ 
+    /* Fills the m_filledPixels data member with only the pixels with data counts */
+    void identifyFilledPixels();
+ 
+
+    void computeCountsSpectrum();
+    
+    void computeCountsSpectrum_wcs();
+    
+    void computeCountsSpectrum_healpix();
+
+ 
+  protected:
+    
+    /// Disable assignement operator
+    BinnedCountsCache & operator=(const BinnedCountsCache & rhs) {
+      throw std::runtime_error("Copy-assignment operator of BinnedCountsCache not implemented");
+    }
+    
+    /// Disable clone function
+    virtual BinnedCountsCache * clone() const {
+      return new BinnedCountsCache(*this);
+    }
+    
+    
+  private:
+    
+    /// Compute the log of rations between energy bin edges
+    static void log_energy_ratios(const std::vector<double>& energies,
+				  std::vector<double>& log_ratios);
+    
+    /* ---------------- Data Members --------------------- */
+    
+    /* ---------------- Data and binning --------------------- */
+    
+    /// The observed data.  Used to provide the binning.
+    CountsMapBase& m_dataMap;
+
+    /* ---------------- Counts Spectra ----------------------- */
+    
+    /// The observed counts spectrum.  
+    /// I.e., the data summed of the ROI for each energy bin
+    std::vector<double> m_countsSpectrum;
+    
+    /// The weighted observed counts spectrum.  
+    /// I.e., the data summed of the ROI for each energy bin
+    std::vector<double> m_countsSpectrum_wt;
+    
+    /// The number of pixels in the counts map
+    size_t m_numPixels; 
+    
+    /// Log of ratios between energy bin edges
+    std::vector<double> m_log_energy_ratios;
+    
+    /* ---------------- Stuff for weighted likelihood --------------- */
+    
+    /// Weights map.  Null ptr -> don't use weights.
+    const ProjMap* m_weightMap_orig;
+    
+    /// Weights map reprojected into counts map binning.  Null ptr -> don't use weights 
+    WeightMap* m_weightMap;
+    
+    /// Map of the weighted counts
+    CountsMapBase* m_weightedCounts;
+
+    /// These are the indices of the pixels with counts     
+    /// This is used to speed up the evaluation of the log-likelihood
+    std::vector<unsigned int> m_filledPixels;
+   
+  };
+
+}
+
+#endif // Likelihood_BinnedCountsCache_h
---
+++ src/BinnedCountsCache.cxx
@@ -0,0 +1,178 @@
+/**
+ * @file BinnedCountsCache.cxx
+ * @brief  
+ * @author 
+ *
+ * $Header: /nfs/slac/g/glast/ground/cvs/Likelihood/src/BinnedLikelihood.cxx,v 1.130 2016/09/29 00:30:25 echarles Exp $
+ */
+
+#include "Likelihood/BinnedCountsCache.h"
+
+#include <stdexcept>
+
+#include "Likelihood/WeightMap.h"
+#include "Likelihood/FileUtils.h"
+
+namespace Likelihood {
+
+  BinnedCountsCache::BinnedCountsCache(CountsMapBase & dataMap,
+				       const Observation & observation,
+				       const ProjMap* weightMap,
+				       const std::string& srcMapsFile)
+    :m_dataMap(dataMap), 
+     m_numPixels(dataMap.pixels().size()),
+     m_weightMap_orig(weightMap),
+     m_weightMap(0),
+     m_weightedCounts(0) {
+    
+    log_energy_ratios(m_dataMap.energies(),m_log_energy_ratios);
+  
+    if ( weightMap != 0 ) {
+      if ( FileUtils::fileHasExtension(srcMapsFile,"__weights__") ) {
+	st_stream::StreamFormatter formatter("BinnedLikelihood","", 2);
+	formatter.warn() << "Reading existing weights map from file " << srcMapsFile << std::endl;
+	m_weightMap = new WeightMap(srcMapsFile, &dataMap, observation);
+      } else {
+	m_weightMap = new WeightMap(*weightMap,&dataMap,observation,true);
+      }
+    }
+    identifyFilledPixels();  
+    computeCountsSpectrum();
+  }
+
+  BinnedCountsCache::~BinnedCountsCache() {
+    delete m_weightMap;
+    delete m_weightedCounts;
+  }
+
+
+  void BinnedCountsCache::setCountsMap(const std::vector<float> & counts) {
+    if(counts.size() != m_dataMap.data().size())
+      throw std::runtime_error("Wrong size for input counts map.");
+    m_dataMap.setImage(counts);
+    identifyFilledPixels();
+    computeCountsSpectrum();
+  }
+
+  tip::Extension* BinnedCountsCache::saveWeightsMap(const std::string& srcMapsFile, bool replace) const {
+    if ( m_weightMap == 0 ) return 0;
+    bool has_weights = FileUtils::fileHasExtension(srcMapsFile,"__weights__");    
+    if ( has_weights ) {
+      if ( replace ) {
+	return FileUtils::replace_image_from_float_vector(srcMapsFile,"__weights__",
+							  m_dataMap,m_weightMap->model(),false);
+      } else {
+	// just leave it be;
+	;
+      }
+    } else {
+      return FileUtils::append_image_from_float_vector(srcMapsFile,"__weights__",
+						       m_dataMap,m_weightMap->model(),false);	
+    }
+    return 0;
+  }
+
+  void BinnedCountsCache::fillWeightedCounts() {
+    if ( m_weightMap == 0 ) {
+      delete m_weightedCounts;
+      m_weightedCounts = 0;
+      return;
+    }
+    delete m_weightedCounts;
+    m_weightedCounts = m_dataMap.clone();
+    // FIXME, this would be more efficient if CountsMapBase had a multiply by function
+    size_t ne = num_ebins();
+    size_t npix = num_pixels();
+    std::vector<float> wts(ne*npix);
+    for ( size_t j(0); j < npix; j++ ) {
+      for (size_t k(0); k < ne; k++) {
+	size_t idx = k*npix +j;
+	double w = m_weightMap->model()[idx];
+	bool is_null = w <= 0 || m_dataMap.data()[idx] <= 0;
+	w = is_null ? 0 : w;
+	wts[idx] = m_dataMap.data()[idx] * w;
+      }
+    }
+    m_weightedCounts->setImage(wts);
+  }
+
+  void BinnedCountsCache::identifyFilledPixels() {
+    if ( has_weights()  ) {
+      fillWeightedCounts();
+    }
+    const std::vector<float> & the_data = data(has_weights());
+    m_filledPixels.clear();
+    for (unsigned int i = 0; i < the_data.size(); i++) {
+      if (the_data.at(i) > 0) {
+	m_filledPixels.push_back(i);
+      }
+    }
+  }
+
+  void BinnedCountsCache::log_energy_ratios(const std::vector<double>& energies,
+					   std::vector<double>& log_ratios) {
+    log_ratios.resize(energies.size() -1);
+    for ( size_t i(0); i < log_ratios.size(); i++ ) {
+      log_ratios[i] = std::log(energies[i+1] / energies[i]);
+    }
+  }
+  
+  void BinnedCountsCache::computeCountsSpectrum() {
+    // EAC_FIX, CountsMap should be able to do this 
+    switch ( m_dataMap.projection().method() ) {
+    case astro::ProjBase::WCS:
+      computeCountsSpectrum_wcs();
+      return;
+    case astro::ProjBase::HEALPIX:
+      computeCountsSpectrum_healpix();
+      return;
+    default:
+      break;
+    }
+    std::string errMsg("BinnedLikelihood did not recognize projection method used for CountsMap: ");
+    errMsg += m_dataMap.filename();
+    throw std::runtime_error(errMsg);
+  }
+
+  void BinnedCountsCache::computeCountsSpectrum_wcs() {
+    m_countsSpectrum.clear();
+    m_countsSpectrum_wt.clear();
+    size_t nx(m_dataMap.imageDimension(0));
+    size_t ny(m_dataMap.imageDimension(1));
+    size_t nz(m_dataMap.imageDimension(2));
+    size_t indx(0);
+    for (size_t k = 0; k < nz; k++) {
+      double ntot(0);
+      double ntot_wt(0);
+      for (size_t j = 0; j < ny; j++) {
+	for (size_t i = 0; i < nx; i++) {
+	  ntot += data(false)[indx];
+	  ntot_wt += data(has_weights())[indx];
+	  indx++;
+	}
+      }
+      m_countsSpectrum.push_back(ntot);
+      m_countsSpectrum_wt.push_back(ntot_wt);
+    }
+  }
+
+  void BinnedCountsCache::computeCountsSpectrum_healpix() {
+    m_countsSpectrum.clear();
+    m_countsSpectrum_wt.clear();
+    size_t nx(m_dataMap.imageDimension(0));
+    size_t ny(m_dataMap.imageDimension(1));
+    size_t indx(0);
+    for (size_t k = 0; k < ny; k++) {
+      double ntot(0);
+      double ntot_wt(0);
+      for (size_t i = 0; i < nx; i++) {
+	ntot += data(false)[indx];
+	ntot_wt += data(has_weights())[indx];
+	indx++;
+      }
+      m_countsSpectrum.push_back(ntot);
+      m_countsSpectrum_wt.push_back(ntot_wt);
+    }
+  }
+
+} // namespace Likelihood
