5c16190e76214fae7b3348afb8d33c7d3e374a6f
jasercion <jasercion.gwu@gmail.com>
2024-04-26 13:17:03 -0400

    Fix Python import

--- src/likelihood/matplotlibcpp.h
+++ src/likelihood/matplotlibcpp.h
@@ -2,7 +2,7 @@
 
 // Python headers must be included before any system headers, since
 // they define _POSIX_C_SOURCE
-#include <python3.9/Python.h>
+#include <Python.h>
 
 #include <vector>
 #include <map>
